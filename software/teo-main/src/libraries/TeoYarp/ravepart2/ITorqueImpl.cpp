d2c0b0cfa882df67d07278ecec5da96db648664d
jgvictores <jgvictores@3ee78acd-4d09-4874-990d-b3b85ff74acd>
2015-01-12 08:37:43 +0000

    =
    Former-commit-id: 6dc7b8797dafc94f93987d9444bff726d89cb150

--- software/teo-main/src/libraries/TeoYarp/ravepart2/ITorqueImpl.cpp
+++ software/teo-main/src/libraries/TeoYarp/ravepart2/ITorqueImpl.cpp
@@ -1,171 +1,171 @@
 // -*- mode:C++; tab-width:4; c-basic-offset:4; indent-tabs-mode:nil -*-
 
-#include "RavePart.hpp"
+#include "RavePart2.hpp"
 
 // ------------------- IForceControl Related ------------------------------------
 
 
-bool teo::RavePart::setTorqueMode() {
+bool teo::RavePart2::setTorqueMode() {
     CD_INFO("setPositionMode()\n");
     return true;
 }
 
 // -----------------------------------------------------------------------------
 
-bool teo::RavePart::getRefTorques(double *t){
+bool teo::RavePart2::getRefTorques(double *t){
     return true;
 }
 
 // -----------------------------------------------------------------------------
 
-bool teo::RavePart::getRefTorque(int j, double *t) {
+bool teo::RavePart2::getRefTorque(int j, double *t) {
     return true;
 }
 
 // -----------------------------------------------------------------------------
 
-bool teo::RavePart::setRefTorques(const double *t) {
+bool teo::RavePart2::setRefTorques(const double *t) {
     return true;
 }
 
 
 // -----------------------------------------------------------------------------
 
-bool teo::RavePart::setRefTorque(int j, double t) {
+bool teo::RavePart2::setRefTorque(int j, double t) {
     CD_INFO("joint: %d, refTorque: %f.\n",j,t);
     return true;
 }
 
 // -----------------------------------------------------------------------------
 
-bool teo::RavePart::getBemfParam(int j, double *bemf) {
+bool teo::RavePart2::getBemfParam(int j, double *bemf) {
     return true;
 }
 
 // -----------------------------------------------------------------------------
 
-bool teo::RavePart::setBemfParam(int j, double bemf) {
+bool teo::RavePart2::setBemfParam(int j, double bemf) {
     return true;
 }
 
 // -----------------------------------------------------------------------------
 
-bool teo::RavePart::setTorquePid(int j, const Pid &pid) {
+bool teo::RavePart2::setTorquePid(int j, const Pid &pid) {
     return true;
 }
 
 // -----------------------------------------------------------------------------
 
-bool teo::RavePart::getTorque(int j, double *t) {
+bool teo::RavePart2::getTorque(int j, double *t) {
     CD_INFO("joint: %d.\n",j);
     return true;
 }
 
 // -----------------------------------------------------------------------------
 
-bool teo::RavePart::getTorques(double *t) {
+bool teo::RavePart2::getTorques(double *t) {
     return true;
 }
 
 // -----------------------------------------------------------------------------
 
-bool teo::RavePart::getTorqueRange(int j, double *min, double *max) {
+bool teo::RavePart2::getTorqueRange(int j, double *min, double *max) {
     return true;
 }
 
 // -----------------------------------------------------------------------------
 
-bool teo::RavePart::getTorqueRanges(double *min, double *max) {
+bool teo::RavePart2::getTorqueRanges(double *min, double *max) {
     return true;
 }
 
 // -----------------------------------------------------------------------------
 
-bool teo::RavePart::setTorquePids(const Pid *pids) {
+bool teo::RavePart2::setTorquePids(const Pid *pids) {
     return true;
 }
 
 // -----------------------------------------------------------------------------
 
-bool teo::RavePart::setTorqueErrorLimit(int j, double limit) {
+bool teo::RavePart2::setTorqueErrorLimit(int j, double limit) {
     return true;
 }
 
 // -----------------------------------------------------------------------------
 
-bool teo::RavePart::setTorqueErrorLimits(const double *limits) {
+bool teo::RavePart2::setTorqueErrorLimits(const double *limits) {
     return true;
 }
 
 // -----------------------------------------------------------------------------
 
-bool teo::RavePart::getTorqueError(int j, double *err) {
+bool teo::RavePart2::getTorqueError(int j, double *err) {
     return true;
 }
 
 // -----------------------------------------------------------------------------
 
-bool teo::RavePart::getTorqueErrors(double *errs) {
+bool teo::RavePart2::getTorqueErrors(double *errs) {
     return true;
 }
 
 // -----------------------------------------------------------------------------
 
-bool teo::RavePart::getTorquePidOutput(int j, double *out) {
+bool teo::RavePart2::getTorquePidOutput(int j, double *out) {
     return true;
 }
 
 // -----------------------------------------------------------------------------
 
-bool teo::RavePart::getTorquePidOutputs(double *outs) {
+bool teo::RavePart2::getTorquePidOutputs(double *outs) {
     return true;
 }
 
 // -----------------------------------------------------------------------------
 
-bool teo::RavePart::getTorquePid(int j, Pid *pid) {
+bool teo::RavePart2::getTorquePid(int j, Pid *pid) {
     return true;
 }
 
 // -----------------------------------------------------------------------------
 
-bool teo::RavePart::getTorquePids(Pid *pids){
+bool teo::RavePart2::getTorquePids(Pid *pids){
     return true;
 }
 
 // -----------------------------------------------------------------------------
 
-bool teo::RavePart::getTorqueErrorLimit(int j, double *limit) {
+bool teo::RavePart2::getTorqueErrorLimit(int j, double *limit) {
     return true;
 }
 
 // -----------------------------------------------------------------------------
 
-bool teo::RavePart::getTorqueErrorLimits(double *limits) {
+bool teo::RavePart2::getTorqueErrorLimits(double *limits) {
     return true;
 }
 
 // -----------------------------------------------------------------------------
 
-bool teo::RavePart::resetTorquePid(int j) {
+bool teo::RavePart2::resetTorquePid(int j) {
     return true;
 }
 
 // -----------------------------------------------------------------------------
 
-bool teo::RavePart::disableTorquePid(int j) {
+bool teo::RavePart2::disableTorquePid(int j) {
     return true;
 }
 
 // -----------------------------------------------------------------------------
 
-bool teo::RavePart::enableTorquePid(int j) {
+bool teo::RavePart2::enableTorquePid(int j) {
     return true;
 }
 
 // -----------------------------------------------------------------------------
 
-bool teo::RavePart::setTorqueOffset(int j, double v) {
+bool teo::RavePart2::setTorqueOffset(int j, double v) {
     return true;
 }
 
