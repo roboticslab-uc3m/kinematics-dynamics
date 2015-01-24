764c5596aea9ec521556b895978b5126b56b6a0f
jgvictores <jgvictores@3ee78acd-4d09-4874-990d-b3b85ff74acd>
2015-01-24 17:41:41 +0000

    =
    Former-commit-id: dab6779960e3dbd9cecdbbf6e30e7040448d4776 [formerly ce9ff184e565f6b11b3b5c88b1ab181c79a27f41] [formerly ebcffe186c9bdcf1a19a9b8f4189b6572d38b48d [formerly 5ad15aa9f4c5b0d662e24eb8cf982cc6b8f663f4]]
    Former-commit-id: d16b78a32fbd908582f809ee9f9ea319da3529d4 [formerly c9d222fa3e5a61c8c0051e65a93fc808571ddf36]
    Former-commit-id: dd6f53ac90e53bfa8e6e2c1d49da5f9f43b4eacb

---
+++ software/teo-main/src/libraries/TeoYarp/kdlsolver/KdlSolver.cpp
@@ -0,0 +1,86 @@
+// -----------------------------------------------------------------------------
+
+#include "KdlSolver.hpp"
+
+// -----------------------------------------------------------------------------
+
+bool teo::KdlSolver::getMatrixFromProperties(Searchable &options, ConstString &tag, yarp::sig::Matrix &H) {
+
+    Bottle *bH=options.find(tag).asList();
+    if (!bH) return false;
+    
+    int i=0;
+    int j=0;
+    H.zero();
+    for (int cnt=0; (cnt<bH->size()) && (cnt<H.rows()*H.cols()); cnt++) {    
+        H(i,j)=bH->get(cnt).asDouble();
+        if (++j>=H.cols()) {
+            i++;
+            j=0;
+        }
+    }
+    return true;
+}
+
+// -----------------------------------------------------------------------------
+
+bool teo::KdlSolver::fwdKin(const yarp::sig::Vector &inUnits, yarp::sig::Vector &x, yarp::sig::Vector &o) {
+    //JntArray inRad = JntArray(cmcNumMotors);
+    JntArray inRad = JntArray(vectorOfCmcMotorIdxs.size());
+    Frame fOutCart;
+    for (int idxIdx=0; idxIdx<vectorOfCmcMotorIdxs.size(); idxIdx++) {
+        int motor = vectorOfCmcMotorIdxs[idxIdx];
+        if(isPrismatic[idxIdx]) inRad(idxIdx)=inUnits[motor];
+        else inRad(idxIdx)=toRad(inUnits[motor]);
+    }
+    ChainFkSolverPos_recursive fksolver = ChainFkSolverPos_recursive(theChain);
+    fksolver.JntToCart(inRad,fOutCart);
+
+    x.clear();
+    x.push_back(fOutCart.p.data[0]);  // pushed on as [0]
+    x.push_back(fOutCart.p.data[1]);  // pushed on as [1]
+    x.push_back(fOutCart.p.data[2]);  // pushed on as [2]
+
+    if (angleRepr == "axisAngle") {
+        o.resize(4);
+        KDL::Vector rotVector = fOutCart.M.GetRot();
+        o[3] = fOutCart.M.GetRotAngle(rotVector);  // Normalizes as colateral effect
+        o[0] = rotVector[0];
+        o[1] = rotVector[1];
+        o[2] = rotVector[2];
+        CD_INFO("KDL computed current cart: %f %f %f | %f %f %f %f.\n",
+            fOutCart.p.data[0],fOutCart.p.data[1],fOutCart.p.data[2],o[0],o[1],o[2],o[3]);
+    } else if (angleRepr == "eulerYZ") {  // ASIBOT
+        double alfa, beta, gamma;
+        fOutCart.M.GetEulerZYZ(alfa, beta, gamma);
+        o.clear();
+        o.push_back(toDeg(beta));  // pushed on as [0]
+        o.push_back(toDeg(gamma));  // pushed on as [1]
+        CD_INFO("KDL computed current cart: %f %f %f | %f %f.\n",
+            fOutCart.p.data[0],fOutCart.p.data[1],fOutCart.p.data[2],o[0],o[1]);
+    } else if (angleRepr == "eulerZYZ") {
+        double alfa, beta, gamma;
+        fOutCart.M.GetEulerZYZ(alfa, beta, gamma);
+        o.clear();
+        o.push_back(toDeg(alfa));  // pushed on as [0]
+        o.push_back(toDeg(beta));  // pushed on as [1]
+        o.push_back(toDeg(gamma));  // pushed on as [2]
+        CD_INFO("KDL computed current cart: %f %f %f | %f %f %f.\n",
+            fOutCart.p.data[0],fOutCart.p.data[1],fOutCart.p.data[2],o[0],o[1],o[2]);
+    } else if (angleRepr == "RPY") {
+        double alfa, beta, gamma;
+        fOutCart.M.GetRPY(alfa, beta, gamma);
+        o.clear();
+        o.push_back(toDeg(alfa));  // pushed on as [0]
+        o.push_back(toDeg(beta));  // pushed on as [1]
+        o.push_back(toDeg(gamma));  // pushed on as [2]
+        CD_INFO("KDL computed current cart: %f %f %f | %f %f %f.\n",
+            fOutCart.p.data[0],fOutCart.p.data[1],fOutCart.p.data[2],o[0],o[1],o[2]);
+    } else {
+        CD_INFO("KDL computed current cart: %f %f %f\n",fOutCart.p.data[0],fOutCart.p.data[1],fOutCart.p.data[2]);
+    }
+
+    return true;
+}
+
+// -----------------------------------------------------------------------------
