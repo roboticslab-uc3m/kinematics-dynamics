af9af3123ef3b5c0d60e8afad0a35dfd9d07de76
Juan G. Victores <jgvictores@users.noreply.github.com>
2015-01-25 18:02:14 +0000

    =
    Former-commit-id: 7408b7a7ab06eba9b510a27d01d1137e3b3dedd8 [formerly b72ead2569614351dd7396f3e3a0b1b74a04bea8] [formerly a711ab1d5c1d2bf3b7b5e05c52cbd9e9fc878433 [formerly a711ab1d5c1d2bf3b7b5e05c52cbd9e9fc878433 [formerly a711ab1d5c1d2bf3b7b5e05c52cbd9e9fc878433 [formerly 1329071639df104490a8345a879312d2b5924ab0]]]]
    Former-commit-id: c70324f763cd7968d6fc153c77e43ecd597b2f95 [formerly 7659eb6520e6a8867d553f916efd2f2bca7ff09b]
    Former-commit-id: c0c19311dd261d2a73a1d2ad9766c8d19c7e9330
    Former-commit-id: 307952b7ef7dab363d2f6c5a3ecf925c246bd684
    Former-commit-id: dcc9454bc9b59ac056657261b7c8fcbf7df0e6af

--- test/testKdlSolver.cpp
+++ test/testKdlSolver.cpp
@@ -39,14 +39,22 @@ TEST_F( KdlSolverTest, KdlSolverFwdKin)
     ASSERT_EQ(true, dd.isValid() );
     dd.view(iCartesianSolver);
     ASSERT_NE((teo::ICartesianSolver*)NULL, iCartesianSolver );
-    std::vector<double> q,x,o;
-    q.push_back(0.0);
+    std::vector<double> q(1),x,o;
+
+    //-- Cast to int to allow e-16 deviations
+    q[0]=0.0;
     iCartesianSolver->fwdKin(q,x,o);
     ASSERT_EQ(x.size(), 3 );
-    ASSERT_EQ(x[0], 1 );
-    ASSERT_EQ(x[1], 0 );
-    ASSERT_EQ(x[2], 0 );
+    ASSERT_EQ((int)x[0], 1 );
+    ASSERT_EQ((int)x[1], 0 );
+    ASSERT_EQ((int)x[2], 0 );
 
+    q[0]=90.0;
+    iCartesianSolver->fwdKin(q,x,o);
+    ASSERT_EQ(x.size(), 3 );
+    ASSERT_EQ((int)x[0], 0  );
+    ASSERT_EQ((int)x[1], 1 );
+    ASSERT_EQ((int)x[2], 0 );
 }
 
 
