e0fee1567ae08f09a2f693f2820c392b0040572d
jgvictores <jgvictores@3ee78acd-4d09-4874-990d-b3b85ff74acd>
2015-01-25 11:17:51 +0000

    =
    Former-commit-id: 2dd6f069b968fc2ef7111134988d1c3dce3cd315 [formerly da01525191cac0c879708afeb60066cacfa4c55f] [formerly 60299c71ec104cb53093d9acec170cb5fa500530 [formerly 67c698a33f84493981058d3d97dd04eca96e38f5]]
    Former-commit-id: f3e8011b5d5605a41c28c26ca1531f2dd45d64a7 [formerly e1b24ae40a975aef1ea7d676058dc447fc59e59b]
    Former-commit-id: f4ac325efac158917050d7694e977af67bb1f105

--- software/teo-main/src/libraries/TeoYarp/kdlsolver/KdlSolver.cpp
+++ software/teo-main/src/libraries/TeoYarp/kdlsolver/KdlSolver.cpp
@@ -4,26 +4,6 @@
 
 // -----------------------------------------------------------------------------
 
-bool teo::KdlSolver::getMatrixFromProperties(Searchable &options, ConstString &tag, yarp::sig::Matrix &H) {
-
-    Bottle *bH=options.find(tag).asList();
-    if (!bH) return false;
-    
-    int i=0;
-    int j=0;
-    H.zero();
-    for (int cnt=0; (cnt<bH->size()) && (cnt<H.rows()*H.cols()); cnt++) {    
-        H(i,j)=bH->get(cnt).asDouble();
-        if (++j>=H.cols()) {
-            i++;
-            j=0;
-        }
-    }
-    return true;
-}
-
-// -----------------------------------------------------------------------------
-
 bool teo::KdlSolver::fwdKin(const yarp::sig::Vector &inUnits, yarp::sig::Vector &x, yarp::sig::Vector &o) {
 
     JntArray inRad = JntArray(numLinks);
