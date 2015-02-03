74cbb56144573ed8fee6334aa96a5d34dcc12fce
Juan G. Victores <jgvictores@users.noreply.github.com>
2015-02-03 08:42:49 +0000

    =
    Former-commit-id: 477e42e69840e40656ae2a9fad2e3da030670fb1 [formerly cde717b61b55499062c9130a582abb300262b372] [formerly ce9cf9bd43113f5bd32f563a7627948989446713 [formerly ce9cf9bd43113f5bd32f563a7627948989446713 [formerly ce9cf9bd43113f5bd32f563a7627948989446713 [formerly 0d6c5d27169c2f8526d84131b381e23141aafe3d]]]]
    Former-commit-id: e3704a952f60f0abbf718f201cfb53c3b03ad0b2 [formerly 6f9649e46ac1798e3673b55af0f9206e89c56bd8]
    Former-commit-id: 962c8a94f08a0cff4a5525e28cc36047af2e77bf
    Former-commit-id: 345f5b68737f36a67b7ed087e7175e02e6343f66
    Former-commit-id: 2af897485cee5f01106a6414584950b66eded707

---
+++ scripts/update-dox.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+# Thanks: http://stackoverflow.com/questions/14710257/running-a-cron-job-at-230-am-every-day
+# On how to automate process at 2:30 every day (type "date" to get your server time)
+# crontab -e
+# 30 2 * * * /your/command
+
+echo "Update TEOrepo..."
+cd $TEO_ROOT
+svn up --username wikirobots
+
+echo "Doxy teo-main..."
+cd teo-main/doc
+rm -r html
+/usr/local/bin/doxygen
+cd ../..
+
+echo "Doxy teo-body..."
+cd teo-body/doc
+rm -r html
+/usr/local/bin/doxygen
+cd ../..
+
+echo "Doxy teo-head..."
+cd teo-head/doc
+rm -r html
+/usr/local/bin/doxygen
+cd ../..
+
