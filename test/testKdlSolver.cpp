0f8c29cbcc37d4ada6876545d04c5fd96574908d
jgvictores <jgvictores>
2015-02-17 12:39:01 +0100

    chop tests

--- test/testKdlSolver.cpp
+++ test/testKdlSolver.cpp
@@ -42,42 +42,46 @@ class KdlSolverTest : public testing::Test
         teo::ICartesianSolver *iCartesianSolver;
 };
 
-TEST_F( KdlSolverTest, KdlSolverFwdKin)
+TEST_F( KdlSolverTest, KdlSolverFwdKin1)
 {
     std::vector<double> q(1),x,o;
-
     q[0]=0.0;
     iCartesianSolver->fwdKin(q,x,o);
     ASSERT_EQ(x.size(), 3 );
     ASSERT_NEAR(x[0], 1, 1e-9);
     ASSERT_NEAR(x[1], 0, 1e-9);
     ASSERT_NEAR(x[2], 0, 1e-9);
+}
 
+TEST_F( KdlSolverTest, KdlSolverFwdKin2)
+{
+    std::vector<double> q(1),x,o;
     q[0]=90.0;
     iCartesianSolver->fwdKin(q,x,o);
     ASSERT_EQ(x.size(), 3 );
     ASSERT_NEAR(x[0], 0, 1e-9);
     ASSERT_NEAR(x[1], 1, 1e-9);
     ASSERT_NEAR(x[2], 0, 1e-9);
-
 }
 
-TEST_F( KdlSolverTest, KdlSolverInvKin)
+TEST_F( KdlSolverTest, KdlSolverInvKin1)
 {
     std::vector<double> xd(3),od(4),qGuess(1),q;
-
     xd[0] = 1;
     xd[1] = 0;
     xd[2] = 0;
     qGuess[0] = 0;
     iCartesianSolver->invKin(xd,od,qGuess,q);
     ASSERT_NEAR(q[0], 0, 1e-3);
+}
 
+TEST_F( KdlSolverTest, KdlSolverInvKin2)
+{
+    std::vector<double> xd(3),od(4),qGuess(1),q;
     xd[0] = 0;
     xd[1] = 1;
     xd[2] = 0;
     qGuess[0] = 0;
     iCartesianSolver->invKin(xd,od,qGuess,q);
     ASSERT_NEAR(q[0], 90, 1e-3);
-
 }
