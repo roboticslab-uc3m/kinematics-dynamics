0335a030deae61493a5c265ff8f5096126de565b
teo <jgvictores@github.com>
2015-03-10 11:04:41 +0100

    fix and clean for working controlboardwrapper2

--- src/libraries/TeoYarp/ravepart/IEncoderImpl.cpp
+++ src/libraries/TeoYarp/ravepart/IEncoderImpl.cpp
@@ -81,3 +81,24 @@ bool teo::RavePart::getEncoderAccelerations(double *accs) {
 
 // -----------------------------------------------------------------------------
 
+bool teo::RavePart::getEncodersTimed(double *encs, double *time) {
+    //CD_INFO("\n");  //-- Way too verbose
+
+    bool ok = true;
+    for(unsigned int i=0; i < axes; i++)
+        ok &= getEncoderTimed(i,&(encs[i]),&(time[i]));
+    return ok;
+}
+
+// -----------------------------------------------------------------------------
+
+bool teo::RavePart::getEncoderTimed(int j, double *encs, double *time) {
+    //CD_INFO("(%d)\n",j);  //-- Way too verbose
+
+    getEncoder(j, encs);
+    *time = yarp::os::Time::now();
+
+    return true;
+}
+
+// -----------------------------------------------------------------------------
