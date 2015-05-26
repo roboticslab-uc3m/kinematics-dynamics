0c3c4cded34c30944006e2e749a2db02b89aa084
jgvictores <jgvictores@github.com>
2015-05-26 12:52:54 +0200

    rename RavePart.hpp to FakeControlboard.hpp and update links
    Former-commit-id: 5e9606edfa48959abe656964f6191ec828100fc6 [formerly 8de80b8152ab3a9113ede12ba2fbf710d1bc2613] [formerly d57fad640384740803bc0f807762d84eb161d6c4 [formerly d57fad640384740803bc0f807762d84eb161d6c4 [formerly d57fad640384740803bc0f807762d84eb161d6c4 [formerly 9761b726a5a6bb75993efab5a37acb301bbbf066]]]]
    Former-commit-id: ac949bfecb3a1619cf6e64e0df334f589ccf1bfc [formerly 6409c02f3a628e44aa5a5eb84a4276cc559fcb1b]
    Former-commit-id: 43fda5a9be7112d3089f85197634ff6da20d3f63
    Former-commit-id: d369884b057d6b07951928731111b879a09ff07f
    Former-commit-id: 7e9d4735b67483517601ed9c9164054f5815ca97

--- libraries/TeoYarp/FakeControlboard/IEncodersImpl.cpp
+++ libraries/TeoYarp/FakeControlboard/IEncodersImpl.cpp
@@ -1,6 +1,6 @@
 // -*- mode:C++; tab-width:4; c-basic-offset:4; indent-tabs-mode:nil -*-
 
-#include "RavePart.hpp"
+#include "FakeControlboard.hpp"
 
 // ------------------ IEncoders Related -----------------------------------------
 
