bfe25cff99307b83377f02547a1a7c0a7dcc95f8
jgvictores <jgvictores@github.com>
2015-05-26 12:43:15 +0200

    separate IEncodersImpl.cpp in itself and IEncodersTimedImpl.cpp
    Former-commit-id: 9aeb8a93e27158064aeccb5106077cb114b81099 [formerly e328a7750cf87e7f232f4674361c0d6f3568f2bc] [formerly 6a0e0aa7bc1e5bafeb1849fa534f2417b76946a2 [formerly 6a0e0aa7bc1e5bafeb1849fa534f2417b76946a2 [formerly 6a0e0aa7bc1e5bafeb1849fa534f2417b76946a2 [formerly 1276fe07c0651d4796a3b557c0152b2b1a047c53]]]]
    Former-commit-id: ccaaf94b02c2eb29bdb1d7fd732fcb42ad103c2d [formerly 4edd1e15cb244bcc021c913bc05e34345e82a496]
    Former-commit-id: f64c5e8a1ba790fce0d9c11da1a86d1850bbf2ef
    Former-commit-id: a7c4e2d18dbbb04c178e5ad6ba2891711c42538a
    Former-commit-id: 324a79149b9cafaef559784eae7eb6f434fec989

---
+++ libraries/TeoYarp/RavePart/IEncodersTimedImpl.cpp
@@ -0,0 +1,27 @@
+// -*- mode:C++; tab-width:4; c-basic-offset:4; indent-tabs-mode:nil -*-
+
+#include "RavePart.hpp"
+
+// ------------------ IEncodersTimed Related -----------------------------------------
+
+bool teo::RavePart::getEncodersTimed(double *encs, double *time) {
+    //CD_INFO("\n");  //-- Way too verbose
+
+    bool ok = true;
+    for(unsigned int i=0; i < axes; i++)
+        ok &= getEncoderTimed(i,&(encs[i]),&(time[i]));
+    return ok;
+}
+
+// -----------------------------------------------------------------------------
+
+bool teo::RavePart::getEncoderTimed(int j, double *encs, double *time) {
+    //CD_INFO("(%d)\n",j);  //-- Way too verbose
+
+    getEncoder(j, encs);
+    *time = yarp::os::Time::now();
+
+    return true;
+}
+
+// -----------------------------------------------------------------------------
