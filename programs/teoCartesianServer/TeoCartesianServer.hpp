f1c473f09fd13b8eeb15d1849d199d646dbb762d
jgvictores <jcgvicto@gmail.com>
2015-07-28 10:44:41 +0200

    Revert "eliminate KdlController and teoCartesianServer"
    This reverts commit 18693e84b57bd0189f99d6960a890ec2e08b7e16.

---
+++ programs/teoCartesianServer/TeoCartesianServer.hpp
@@ -0,0 +1,64 @@
+// -*- mode:C++; tab-width:4; c-basic-offset:4; indent-tabs-mode:nil -*-
+
+#ifndef __CARTESIAN_SERVER__
+#define __CARTESIAN_SERVER__
+
+#include <yarp/os/RFModule.h>
+#include <yarp/os/Module.h>
+#include <yarp/os/Network.h>
+#include <yarp/os/Port.h>
+#include <yarp/os/BufferedPort.h>
+
+#include <yarp/dev/PolyDriver.h>
+#include <yarp/dev/CartesianControl.h>
+#include <yarp/dev/ControlBoardInterfaces.h>
+
+#include "ColorDebug.hpp"
+#include "TeoXRpcResponder.hpp"
+#include "TeoXCallbackPort.hpp"
+
+#define DEFAULT_CONTROLLER "kdlcontroller"
+#define DEFAULT_PREFIX "/teoSim/rightArm"
+#define DEFAULT_MOVJ_LOCAL "/teoSim/rightArm/movjCartesianServer"
+#define DEFAULT_MOVJ_REMOTE "/teoSim/rightArm"
+
+using namespace yarp::os;
+using namespace yarp::dev;
+
+namespace teo
+{
+
+/**
+ * @ingroup TeoCartesianServer
+ *
+ * @brief The TeoCartesianServer class implements a server part that receives a connection from a remote
+ * \ref cartesianServer module.
+ * 
+ */
+class TeoCartesianServer : public RFModule {
+protected:
+    yarp::dev::PolyDriver cartesianDevice;
+    yarp::dev::PolyDriver robotDevice;
+
+    yarp::dev::ICartesianControl *icart;
+    yarp::dev::IPositionControl *ipos;
+    int *csStatus;
+
+    RpcServer xRpcServer;
+	TeoXRpcResponder xResponder;
+
+    TeoXCallbackPort xPort;
+
+    bool updateModule();
+    bool interruptModule();
+    // double getPeriod();
+
+public:
+    TeoCartesianServer();
+    bool configure(ResourceFinder &rf);
+};
+
+}  // namespace teo
+
+#endif
+
