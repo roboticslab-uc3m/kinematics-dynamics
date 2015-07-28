ccb99f86a932a6d3e1092a2ca9c11fd2072c9724
jgvictores <jcgvicto@gmail.com>
2015-07-28 11:25:25 +0200

    cartesian rate thread

---
+++ programs/teoCartesianServer/CartesianRateThread.cpp
@@ -0,0 +1,52 @@
+// -*- mode:C++; tab-width:4; c-basic-offset:4; indent-tabs-mode:nil -*-
+
+#include "CartesianRateThread.hpp"
+
+/************************************************************************/
+bool teo::CartesianRateThread::threadInit() {
+
+    iEncoders->getAxes( &numMotors );
+    CD_INFO("numMotorsRA: %d.\n",numMotors);
+
+    solver->getNumLinks( &solverNumLinks );
+    CD_INFO("solverNumLinksRA: %d.\n",solverNumLinks);
+
+    if( numMotors < solverNumLinks ) {
+        CD_ERROR("numMotorsRA < solverNumLinksRA !!! (must be >=) (RA=rightArm)\n");
+        return false;
+    }
+
+    q.resize( numMotors );
+
+    iTorqueControl->setTorqueMode();
+
+    return true;
+}
+
+/************************************************************************/
+void teo::CartesianRateThread::run() {
+
+    iEncoders->getEncoders( q.data() );
+
+    CD_DEBUG("<-- ");
+    for(int i=0;i<numMotors;i++)
+        CD_DEBUG_NO_HEADER("%f ",q[i]);
+    CD_DEBUG_NO_HEADER("[deg]\n");
+
+    solver->invDyn(q,t);
+
+    if( numMotors > numMotors )
+        t.resize( numMotors );  //-- Extra motors won't care about torques.
+
+    CD_INFO("--> ");
+    for(int i=0;i<numMotors;i++) {
+        CD_INFO_NO_HEADER("%f ",t[i]);
+    }
+    CD_INFO_NO_HEADER("[Nm]\n");
+
+    //--tRA[0] = 0.0;  //-- Release... let's do this!
+    iTorqueControl->setRefTorques( t.data() );
+
+}
+
+/************************************************************************/
---
+++ programs/teoCartesianServer/CartesianRateThread.hpp
@@ -0,0 +1,56 @@
+// -*- mode:C++; tab-width:4; c-basic-offset:4; indent-tabs-mode:nil -*-
+
+#ifndef __CARTESIAN_RATE_THREAD__
+#define __CARTESIAN_RATE_THREAD__
+
+#include <vector>
+
+#include <yarp/os/RateThread.h>
+#include <yarp/dev/ControlBoardInterfaces.h>
+
+#include "ColorDebug.hpp"
+#include "ICartesianSolver.h"
+
+#define DEFAULT_MS 50  // [ms], overwritten by parent DEFAULT_PT_MODE_MS.
+
+namespace teo
+{
+
+/**
+ * @ingroup teoCartesianServer
+ *
+ * @brief The actual \ref teoCartesianServer periodical thread.
+ *
+ */
+class CartesianRateThread : public yarp::os::RateThread {
+
+    public:
+        // Set the Thread Rate in the class constructor
+        CartesianRateThread() : RateThread(DEFAULT_MS) {}  // In ms
+
+        /** Initialization method. */
+        virtual bool threadInit();
+
+        /** Loop function. This is the thread itself. */
+        virtual void run();
+
+        /** Solver stuff */
+        int solverNumLinks;
+        teo::ICartesianSolver *solver;
+
+        /** Robot stuff */
+        int numMotors;
+        yarp::dev::IEncoders *iEncoders;
+        yarp::dev::ITorqueControl *iTorqueControl;
+        std::vector< double > q;
+        std::vector< double > t;
+
+    protected:
+
+
+};
+
+}  // namespace teo
+
+#endif  // __CARTESIAN_RATE_THREAD__
+
