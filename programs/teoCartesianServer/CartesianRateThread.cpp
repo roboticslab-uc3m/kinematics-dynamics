e4f9c9e7c3cb926aa265f1f14b1537e9fda55798
jgvictores <jcgvicto@gmail.com>
2015-07-30 11:39:37 +0200

    cleaner class

--- programs/teoCartesianServer/CartesianRateThread.cpp
+++ programs/teoCartesianServer/CartesianRateThread.cpp
@@ -2,9 +2,12 @@
 
 #include "CartesianRateThread.hpp"
 
+namespace teo
+{
+
 /************************************************************************/
 
-bool teo::CartesianRateThread::threadInit()
+bool CartesianRateThread::threadInit()
 {
     iEncoders->getAxes( &numMotors );
     CD_INFO("numMotors: %d.\n",numMotors);
@@ -23,61 +26,12 @@ bool teo::CartesianRateThread::threadInit()
     xDotDesired.resize( 6 );
     xDotCmd.resize( 6 );
 
-    iVelocityControl->setVelocityMode();
-
-    lineCount = 1;
     return true;
 }
 
 /************************************************************************/
 
-void teo::CartesianRateThread::run()
-{
-    std::string line;
-    if (! getline( ifs, line) )
-    {
-        this->stop();
-        CD_INFO("[L:%d] file ended\n", lineCount );
-        return;
-    }
-
-    CD_DEBUG("[L:%d] %s\n", lineCount,line.c_str() );
-    lineCount++;
-    yarp::os::Bottle lineBottle(line);  //-- yes, using a bottle to parse a string
-
-    //-- xDesired from file
-    for(int i=0; i<xDesired.size(); i++)
-        xDesired[i] = lineBottle.get( i ).asDouble();
-
-    //-- xDotDesired from file
-    for(int i=0; i<xDotDesired.size(); i++)
-        xDotDesired[i] = lineBottle.get( i+xDesired.size() ).asDouble();
-
-    //-- qReal from encoders
-    iEncoders->getEncoders( qReal.data() );
-
-    //-- xError = xDesired - xReal, where xReal is fwdKin of qReal
-    solver->fwdKinError(xDesired,qReal, xError);
-
-    //-- control law in cartesian space
-    for(int i=0; i<xDotCmd.size(); i++)
-        xDotCmd[i] = xDotDesired[i] + DEFAULT_GAIN * xError[i];
-
-    //-- final command to joint space
-    solver->diffInvKin(qReal,xDotCmd, qDotCmd);
-
-    CD_INFO("--> ");
-    for(int i=0; i<qDotCmd.size(); i++) {
-        CD_INFO_NO_HEADER("%f ",qDotCmd[i]);
-    }
-    CD_INFO_NO_HEADER("[deg/s]\n");
-
-    iVelocityControl->velocityMove( qDotCmd.data() );
-}
-
-/************************************************************************/
-
-bool teo::CartesianRateThread::load(const std::string& fileName)
+bool CartesianRateThread::load(const std::string& fileName)
 {
     std::string fullFileName = rf->findFileByName( fileName );
     this->ifs.open( fullFileName.c_str() );
@@ -88,10 +42,63 @@ bool teo::CartesianRateThread::load(const std::string& fileName)
     }
     CD_SUCCESS("Opened file: %s.\n",fileName.c_str());
 
-    //-- Start the thread.
-    CD_INFO("Start thread...\n");
-    this->start();
+    lineCount = 1;
+    play = true;
+
+    iVelocityControl->setVelocityMode();
+
     return true;
 }
 
 /************************************************************************/
+
+void CartesianRateThread::run()
+{
+    if (play)
+    {
+        std::string line;
+        if (! getline( ifs, line) )
+        {
+            this->stop();
+            CD_INFO("[L:%d] file ended\n", lineCount );
+            return;
+        }
+
+        CD_DEBUG("[L:%d] %s\n", lineCount,line.c_str() );
+        lineCount++;
+        yarp::os::Bottle lineBottle(line);  //-- yes, using a bottle to parse a string
+
+        //-- xDesired from file
+        for(int i=0; i<xDesired.size(); i++)
+            xDesired[i] = lineBottle.get( i ).asDouble();
+
+        //-- xDotDesired from file
+        for(int i=0; i<xDotDesired.size(); i++)
+            xDotDesired[i] = lineBottle.get( i+xDesired.size() ).asDouble();
+
+        //-- qReal from encoders
+        iEncoders->getEncoders( qReal.data() );
+
+        //-- xError = xDesired - xReal, where xReal is fwdKin of qReal
+        solver->fwdKinError(xDesired,qReal, xError);
+
+        //-- control law in cartesian space
+        for(int i=0; i<xDotCmd.size(); i++)
+            xDotCmd[i] = xDotDesired[i] + DEFAULT_GAIN * xError[i];
+
+        //-- final command to joint space
+        solver->diffInvKin(qReal,xDotCmd, qDotCmd);
+
+        CD_INFO("--> ");
+        for(int i=0; i<qDotCmd.size(); i++) {
+            CD_INFO_NO_HEADER("%f ",qDotCmd[i]);
+        }
+        CD_INFO_NO_HEADER("[deg/s]\n");
+
+        iVelocityControl->velocityMove( qDotCmd.data() );
+    } //end{if(play)}
+}
+
+/************************************************************************/
+
+}  // namespace teo
