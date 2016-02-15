4fd83c7aef5878eb75ac212f311ecadeedbecfd7
yo <jcgvicto@gmail.com>
2016-02-15 22:32:01 +0100

    add empty implementation of methods
    Former-commit-id: b4a408db1d3d676b09fbe88cad4276e20af5a17c [formerly b34c2c388f442aa9d8b79e44c0f7e385a0904548] [formerly 462dca57d928d7a963ef2753033d14468933ac79 [formerly 462dca57d928d7a963ef2753033d14468933ac79 [formerly 462dca57d928d7a963ef2753033d14468933ac79 [formerly c08febcec1005cb06f9ea146bfb4b6a804a6e751]]]]
    Former-commit-id: a48f146325b253583710ce3034277161cbc2e972 [formerly 44e8e9d6b6b7b0c945d17272dde79c32a96c842f]
    Former-commit-id: b561b869f72aab577ac8208db6758d1e118d1100
    Former-commit-id: b7412798c557d1765f97609a36851ee08931b29a
    Former-commit-id: 39a02d37534b3891d92456a46a2974a4c2e0033f

--- libraries/TeoYarp/FakeControlboard/IPosition2Impl.cpp
+++ libraries/TeoYarp/FakeControlboard/IPosition2Impl.cpp
@@ -4,6 +4,58 @@
 
 // ------------------- IPositionControl2 Related --------------------------------
 
+bool teo::FakeControlboard::positionMove(const int n_joint, const int *joints, const double *refs)
+{
+    return true;
+}
 
 // -----------------------------------------------------------------------------
 
+bool teo::FakeControlboard::relativeMove(const int n_joint, const int *joints, const double *deltas)
+{
+    return true;
+}
+
+// -----------------------------------------------------------------------------
+
+bool teo::FakeControlboard::checkMotionDone(const int n_joint, const int *joints, bool *flags)
+{
+    return true;
+}
+
+// -----------------------------------------------------------------------------
+
+bool teo::FakeControlboard::setRefSpeeds(const int n_joint, const int *joints, const double *spds)
+{
+    return true;
+}
+
+// -----------------------------------------------------------------------------
+
+bool teo::FakeControlboard::setRefAccelerations(const int n_joint, const int *joints, const double *accs)
+{
+    return true;
+}
+
+// -----------------------------------------------------------------------------
+
+bool teo::FakeControlboard::getRefSpeeds(const int n_joint, const int *joints, double *spds)
+{
+    return true;
+}
+
+// -----------------------------------------------------------------------------
+
+bool teo::FakeControlboard::getRefAccelerations(const int n_joint, const int *joints, double *accs)
+{
+    return true;
+}
+
+// -----------------------------------------------------------------------------
+
+bool teo::FakeControlboard::stop(const int n_joint, const int *joints)
+{
+    return true;
+}
+
+// -----------------------------------------------------------------------------
