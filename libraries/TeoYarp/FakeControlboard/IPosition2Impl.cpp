8c830dfcc919915c7ac30dfdbd5d2723a9e14fdd
yo <jcgvicto@gmail.com>
2016-02-15 22:24:31 +0100

    add IPosition2Impl.cpp to git and cmake

---
+++ libraries/TeoYarp/FakeControlboard/IPosition2Impl.cpp
@@ -0,0 +1,9 @@
+// -*- mode:C++; tab-width:4; c-basic-offset:4; indent-tabs-mode:nil -*-
+
+#include "FakeControlboard.hpp"
+
+// ------------------- IPositionControl2 Related --------------------------------
+
+
+// -----------------------------------------------------------------------------
+
