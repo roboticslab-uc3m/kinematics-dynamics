81b316d03cc0852cddd2883f73b55a49e456bfda
jgvictores <jcgvicto@gmail.com>
2016-06-19 20:34:35 +0200

    ok robotDevice viewEncoders
    Former-commit-id: 8f790db2ed65937f464f93389e30482201abb2b2 [formerly 0108d5e2153f61c309403a8e02cdc0e7351c7bba] [formerly 16f55aba917e383a38a6d182e114a21c186d51e0 [formerly 16f55aba917e383a38a6d182e114a21c186d51e0 [formerly 16f55aba917e383a38a6d182e114a21c186d51e0 [formerly 97e056995baeb71080f47623367d39c26bd3ceca]]]]
    Former-commit-id: 2fecf0ada1b0ad2b5648088bf928102e4f2ece1d [formerly 6124e0cab50c87840df0b7a8716c67951636a703]
    Former-commit-id: 5ea788b05da10499f2bce4b2ab6074cc49a568f4
    Former-commit-id: 1c61b58dc8e5da809b8ee83674cea9972c424b54
    Former-commit-id: 858b851bbc2d98cbe16e491776e8fc65a98faf04

--- libraries/TeoYarp/BasicCartesianControl/DeviceDriverImpl.cpp
+++ libraries/TeoYarp/BasicCartesianControl/DeviceDriverImpl.cpp
@@ -6,13 +6,15 @@
 
 bool teo::BasicCartesianControl::open(yarp::os::Searchable& config) {
 
+    CD_DEBUG("BasicCartesianControl config: %s.\n", config.toString().c_str());
+
     std::string solverStr = config.check("solver",yarp::os::Value(DEFAULT_SOLVER),"cartesian solver").asString();
+    std::string robotStr = config.check("robot",yarp::os::Value(DEFAULT_ROBOT),"robot device").asString();
 
     yarp::os::Property solverOptions;
     solverOptions.fromString( config.toString() );
     solverOptions.put("device",solverStr);
 
-    CD_DEBUG("solverOptions: %s.\n", solverOptions.toString().c_str());
     solverDevice.open(solverOptions);
     if( ! solverDevice.isValid() ) {
         CD_ERROR("solverDevice not valid: %s.\n",solverStr.c_str());
@@ -23,6 +25,18 @@ bool teo::BasicCartesianControl::open(yarp::os::Searchable& config) {
         return false;
     }
 
+    yarp::os::Property robotOptions;
+    robotOptions.fromString( config.toString() );
+    robotOptions.put("device",robotStr);
+    robotDevice.open(robotOptions);
+    if( ! robotDevice.isValid() ) {
+        CD_ERROR("robotDevice not valid: %s.\n",robotStr.c_str());
+        return false;
+    }
+    if( ! robotDevice.view(iEncoders) ) {
+        CD_ERROR("Could not view iEncoders in: %s.\n",robotStr.c_str());
+        return false;
+    }
 
     return true;
 }
