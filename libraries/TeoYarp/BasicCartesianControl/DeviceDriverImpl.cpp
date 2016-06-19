086e074a68bd23ff4c64bf0b20088aa6bfd6e9b4
jgvictores <jcgvicto@gmail.com>
2016-06-19 20:14:28 +0200

    begin adding BasicCartesianControl members
    Former-commit-id: dd98610f180f83605c5e8d926ad2b4be284b287d [formerly 7053e1f08e6c1fa9714509a3bb261f541d541d59] [formerly e2ad97344fd3a56a0a38cd5472ed11517d44aed8 [formerly e2ad97344fd3a56a0a38cd5472ed11517d44aed8 [formerly e2ad97344fd3a56a0a38cd5472ed11517d44aed8 [formerly e37ad03f9f81aebabdb86d7ff82bb34a0da9f866]]]]
    Former-commit-id: 0da02315194b3816094377ef15b2002353fce954 [formerly 6588aa0c11184d9b129581c686c0c41db6df803a]
    Former-commit-id: c1adbaaebd4cadd28e623e86d4a71b75f44862d6
    Former-commit-id: 9466e89bdae29ecfa5c828cff0708efcebe24a3a
    Former-commit-id: ac5844745a76fcb0b6f513bc494927c758845da5

--- libraries/TeoYarp/BasicCartesianControl/DeviceDriverImpl.cpp
+++ libraries/TeoYarp/BasicCartesianControl/DeviceDriverImpl.cpp
@@ -6,7 +6,23 @@
 
 bool teo::BasicCartesianControl::open(yarp::os::Searchable& config) {
 
-    //numLinks = config.check("numLinks",yarp::os::Value(DEFAULT_NUM_LINKS),"chain number of segments").asInt();
+    std::string solverStr = config.check("solver",yarp::os::Value(DEFAULT_SOLVER),"cartesian solver").asString();
+
+    yarp::os::Property solverOptions;
+    solverOptions.fromString( config.toString() );
+    solverOptions.put("device",solverStr);
+
+    CD_DEBUG("solverOptions: %s.\n", solverOptions.toString().c_str());
+    solverDevice.open(solverOptions);
+    if( ! solverDevice.isValid() ) {
+        CD_ERROR("solverDevice not valid: %s.\n",solverStr.c_str());
+        return false;
+    }
+    if( ! solverDevice.view(iCartesianSolver) ) {
+        CD_ERROR("Could not view ICartesianSolver in: %s.\n",solverStr.c_str());
+        return false;
+    }
+
 
     return true;
 }
