3c2aecb127c49792693da72da9d449bdb24d2273
jgvictores <jcgvicto@gmail.com>
2016-06-20 12:56:52 +0200

    [CartesianControlServer] move read() to cpp
    Former-commit-id: e6140ad32538133b495f0d3e250c7fad96d48289 [formerly c2a1b64f4ee8d867d16238d6bc1a2084b12f2f82] [formerly 07c74df79e0880828f4ca53f86a69a78b6694d6e [formerly 07c74df79e0880828f4ca53f86a69a78b6694d6e [formerly 07c74df79e0880828f4ca53f86a69a78b6694d6e [formerly cf03f70fe3f0f6e9d9cce36598227b48067f662c]]]]
    Former-commit-id: 181d01f2abd68c3252d2b45804f51a5e4024286b [formerly 6adc18f8133b2baa71cb26c1023075c653961af3]
    Former-commit-id: ca2eadecc8eedd0c44b2428e9c838de8165c2d05
    Former-commit-id: a0456b6feaa5ddc76c41dae0169bfb6f7460a2ec
    Former-commit-id: cb59afc7a7514050c744fcf2fbfb50746d462e6d

--- libraries/TeoYarp/CartesianControlServer/CartesianControlServer.hpp
+++ libraries/TeoYarp/CartesianControlServer/CartesianControlServer.hpp
@@ -60,23 +60,7 @@ class CartesianControlServer : public yarp::dev::DeviceDriver, public yarp::os::
 
         // -------- PortReader declarations. Implementation in CartesianControlServer.cpp --------
 
-        virtual bool read(yarp::os::ConnectionReader& connection)
-        {
-            yarp::os::Bottle in, out;
-            bool ok = in.read(connection);
-            if (!ok) return false;
-
-            // process data "in", prepare "out"
-            CD_DEBUG("Got: %s\n",in.toString().c_str());
-
-            out.addVocab(VOCAB_OK);
-
-            yarp::os::ConnectionWriter *returnToSender = connection.getWriter();
-            if (returnToSender!=NULL) {
-                out.write(*returnToSender);
-            }
-            return true;
-        }
+        virtual bool read(yarp::os::ConnectionReader& connection);
 
     protected:
 
