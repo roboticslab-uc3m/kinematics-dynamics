8099fe77317eeca1ef44d59de3d88f692916fef9
jgvictores <jcgvicto@gmail.com>
2016-06-28 15:51:02 +0200

    add CartesianControlClient. empty but opens port and connects, for #27

---
+++ libraries/TeoYarp/CartesianControlClient/DeviceDriverImpl.cpp
@@ -0,0 +1,45 @@
+// -*- mode:C++; tab-width:4; c-basic-offset:4; indent-tabs-mode:nil -*-
+
+#include "CartesianControlClient.hpp"
+
+// ------------------- DeviceDriver Related ------------------------------------
+
+bool teo::CartesianControlClient::open(yarp::os::Searchable& config) {
+
+    std::string local = config.check("local",yarp::os::Value(DEFAULT_LOCAL),"local").asString();
+    std::string remote = config.check("remote",yarp::os::Value(DEFAULT_REMOTE),"remote").asString();
+
+    local += "/rpc:c";
+    rpcClient.open(local);
+
+    remote += "/rpc:s";
+
+    int tries = 0;
+    while(tries++ < 10)
+    {
+        yarp::os::Network::connect(local,remote);
+        if( rpcClient.getOutputCount() > 0) break;
+        CD_DEBUG("Wait to connect to remote, try %d...\n",tries);
+        yarp::os::Time::delay(0.5);
+    }
+
+    if (tries == 11)
+    {
+        CD_ERROR("Timeout on connect to remote!\n");
+        return false;
+    }
+
+    CD_SUCCESS("Connected to remote.\n");
+
+    return true;
+}
+
+// -----------------------------------------------------------------------------
+
+bool teo::CartesianControlClient::close()
+{
+    rpcClient.close();
+    return true;
+}
+
+// -----------------------------------------------------------------------------
