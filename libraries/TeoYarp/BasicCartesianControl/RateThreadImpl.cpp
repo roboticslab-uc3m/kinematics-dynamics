cb0adb3349a2c47435270595dacc1e6d059c4bcc
jgvictores <jcgvicto@gmail.com>
2016-06-29 14:54:08 +0200

    implement more of BasicCartesianControl movl required functionality
    Former-commit-id: 8c6e4c783e693c3eef80f11e0309b47ef75d84ca [formerly d03f12009ebb496904d47638405ba03c1b76c5c4] [formerly d358b65ea5f896d4845032df5814d26b333268d5 [formerly d358b65ea5f896d4845032df5814d26b333268d5 [formerly d358b65ea5f896d4845032df5814d26b333268d5 [formerly 54d26a96648f5b0ac40de8b30a440147252e2e62]]]]
    Former-commit-id: 8556957f1be211c51355cbe99a4df462c1821007 [formerly f1ad9da95868c705bd2194f4b08b17cf569f9070]
    Former-commit-id: cfcfa012729ef8360e28203769179edab44ee88d
    Former-commit-id: 8668ce280a925a9b68627e01a59dc8be9989ab02
    Former-commit-id: 18fae076f17660571a7b5de2e9a748893fae90e5

--- libraries/TeoYarp/BasicCartesianControl/RateThreadImpl.cpp
+++ libraries/TeoYarp/BasicCartesianControl/RateThreadImpl.cpp
@@ -10,6 +10,39 @@ void teo::BasicCartesianControl::run() {
     {
         double movementTime = yarp::os::Time::now() - movementStartTime;
         CD_DEBUG("MOVEL_CONTROLLING: %f\n",movementTime);
+
+        std::vector<double> currentQ(numRobotJoints), currentX, commandXdot, commandQdot;
+
+        if ( ! iEncoders->getEncoders( currentQ.data() ) )
+        {
+            CD_WARNING("getEncoders failed, not updating control this iteration.\n");
+            return;
+        }
+        if ( ! iCartesianSolver->fwdKin(currentQ,currentX) )
+        {
+            CD_WARNING("fwdKin failed, not updating control this iteration.\n");
+            return;
+        }
+
+        //KDL::Frame desiredF = movementTrajectory->Pos(movementTime);
+        //KDL::Twist desiredT = movementTrajectory->Vel(movementTime);
+
+        //KDL::Twist commandXdot = diff(currentX, targetF);
+        //for(unsigned int i=0; i<6; i++)
+        //{
+        //    commandXdot(i) *= GAIN;
+        //    commandXdot(i) += desiredT(i);
+        //}
+
+        if (! iCartesianSolver->diffInvKin(currentQ,commandXdot,commandQdot) )
+        {
+            CD_WARNING("diffInvKin failed, not updating control this iteration.\n");
+        }
+        if( ! iVelocityControl->velocityMove( commandQdot.data() ) )
+        {
+            CD_WARNING("velocityMove failed, not updating control this iteration.\n");
+        }
+
     }
     return;
 }
