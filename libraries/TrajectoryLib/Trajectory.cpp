52df81144d6c6d41c1a6ba035178850c0781d5b9
jgvictores <jcgvicto@gmail.com>
2016-06-30 12:18:38 +0200

    move Trajectory implementation to cpp, make TrajectoryLib and link BasicCartesianControl to it
    Former-commit-id: 5f82ccde8a6dab766f9eaffd37615578d9c8490d [formerly f70f0b4eb7179312d2367f3dc3a862fc7d5aed9a] [formerly 3e3ce0e46bf1ec0a4d1f081606ff8803d57a379d [formerly 3e3ce0e46bf1ec0a4d1f081606ff8803d57a379d [formerly 3e3ce0e46bf1ec0a4d1f081606ff8803d57a379d [formerly 8d2d7157559b5ad8f1d5b6cce68c6e50c3692fb0]]]]
    Former-commit-id: b3373b7c8972683904e00a2db67d908c974052bd [formerly d8abad4441a496940100b174728b528c21a15c6a]
    Former-commit-id: 4bfbc026be82cc7c51977178e965b898e9016d8c
    Former-commit-id: 9f1896142165d2e91f0d2104273fb01f40b559e8
    Former-commit-id: a7e95a50d6829f3500c07f7ce86b8d5a751a94fd

---
+++ libraries/TrajectoryLib/Trajectory.cpp
@@ -0,0 +1,13 @@
+// -*- mode:C++; tab-width:4; c-basic-offset:4; indent-tabs-mode:nil -*-
+
+#include "Trajectory.hpp"
+
+// -----------------------------------------------------------------------------
+
+bool teo::Trajectory::getX(const double movementTime, std::vector<double>& desiredX)
+{
+        //KDL::Frame desiredX = trajectory->Pos(movementTime);
+        return true;
+}
+
+// -----------------------------------------------------------------------------
