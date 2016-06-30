e7eedaa8f10adb4c4728bade69b5cc3a35c649e7
jgvictores <jcgvicto@gmail.com>
2016-06-30 12:47:41 +0200

    add helper vectorToFrame frameToVector members to Trajectory

--- libraries/TrajectoryLib/Trajectory.hpp
+++ libraries/TrajectoryLib/Trajectory.hpp
@@ -13,12 +13,16 @@
 #include <kdl/velocityprofile_trap.hpp>
 #include <kdl/trajectory_segment.hpp>
 
+#include "ColorDebug.hpp"
+
 namespace teo
 {
 
 class Trajectory
 {
 public:
+
+    Trajectory();
 
     bool getX(const double movementTime, std::vector<double>& desiredX);
 
@@ -31,6 +35,28 @@ private:
     KDL::Trajectory_Segment* currentTrajectory;
     KDL::RotationalInterpolation_SingleAxis* _orient;
 
+    bool vectorToFrame(const std::vector<double> &x, KDL::Frame& f);
+    bool frameToVector(const KDL::Frame& f, std::vector<double> &x);
+
+    std::string angleRepr;
+
+    /**
+    * Simple function to pass from radians to degrees.
+    * @param inRad angle value in radians.
+    * @return angle value in degrees.
+    */
+    double toDeg(const double inRad) {
+        return (inRad * 180.0 / M_PI);  // return (inRad * 180.0 / 3.14159265);
+    }
+
+    /**
+    * Simple function to pass from degrees to radians.
+    * @param inDeg angle value in degrees.
+    * @return angle value in radians.
+    */
+    double toRad(const double inDeg) {
+        return (inDeg * M_PI / 180.0);  // return (inDeg * 3.14159265 / 180.0);
+    }
 };
 
 }  // namespace teo
