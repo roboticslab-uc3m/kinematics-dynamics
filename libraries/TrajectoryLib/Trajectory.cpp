c6038e263b4397812d7d50cc4dcc8a4bdbc1e75b
jgvictores <jcgvicto@gmail.com>
2016-06-30 12:47:41 +0200

    add helper vectorToFrame frameToVector members to Trajectory
    Former-commit-id: e1c9af41bdf21c6bae5c81c9e626910c45dee68f [formerly 9936dec8d1c25220192b3c90b8669080b61517ac] [formerly c654c363f9da594bf397c9e4000499a521100c1e [formerly c654c363f9da594bf397c9e4000499a521100c1e [formerly c654c363f9da594bf397c9e4000499a521100c1e [formerly e7eedaa8f10adb4c4728bade69b5cc3a35c649e7]]]]
    Former-commit-id: b88579ff79d2d0a3dc836914cf69598a177de693 [formerly 8fb9ef9dbc5cf186052c09fd05ffce474390d58e]
    Former-commit-id: edfe13f6b40d74fbb41fbcfaec4a67457d63d97b
    Former-commit-id: 5dad556bfbb69636f5b87ec3677d880da2508fbb
    Former-commit-id: fcd8305b4e2e682a5dbbbe581a46fab857428ea2

--- libraries/TrajectoryLib/Trajectory.cpp
+++ libraries/TrajectoryLib/Trajectory.cpp
@@ -4,6 +4,13 @@
 
 // -----------------------------------------------------------------------------
 
+teo::Trajectory::Trajectory()
+{
+    angleRepr = "axisAngle";
+}
+
+// -----------------------------------------------------------------------------
+
 bool teo::Trajectory::getX(const double movementTime, std::vector<double>& desiredX)
 {
     //KDL::Frame desiredX = trajectory->Pos(movementTime);
@@ -29,6 +36,90 @@ bool teo::Trajectory::generateLine(const std::vector<double> &src, const std::ve
     //KDL::Path_Line testPathLine(initF, targetF, _orient, _eqradius, _aggregate);
     //KDL::VelocityProfile_Trap testVelocityProfile(maxVel, maxAcc);
     //currentTrajectory = new Trajectory_Segment(testPathLine.Clone(), testVelocityProfile.Clone(), duration, _aggregate);
+
+    return true;
+}
+
+// -----------------------------------------------------------------------------
+
+bool teo::Trajectory::vectorToFrame(const std::vector<double> &x, KDL::Frame& f) {
+
+    f.p.data[0]=x[0];
+    f.p.data[1]=x[1];
+    f.p.data[2]=x[2];
+
+    if (angleRepr == "axisAngle") {
+        f.M = KDL::Rotation::Rot(KDL::Vector(x[3],x[4],x[5]),toRad(x[6]));
+    }
+    else if (angleRepr == "eulerYZ")  //-- like ASIBOT
+    {
+        f.M = KDL::Rotation::EulerZYZ(::atan2(x[1],x[0]),toRad(x[3]), toRad(x[4]));
+    }
+    else if (angleRepr == "eulerZYZ")
+    {
+        f.M = KDL::Rotation::EulerZYZ(toRad(x[3]), toRad(x[4]), toRad(x[5]));
+    }
+    else if (angleRepr == "RPY")
+    {
+        f.M = KDL::Rotation::RPY(toRad(x[3]), toRad(x[4]), toRad(x[5]));
+    }
+    else  //-- No known angle repr.
+    {
+        CD_WARNING("angleRepr unknown %s\n",angleRepr.c_str());
+    }
+
+    return true;
+}
+
+// -----------------------------------------------------------------------------
+
+bool teo::Trajectory::frameToVector(const KDL::Frame& f, std::vector<double> &x) {
+
+    //-- Fill angle first, then 0-2 for position.
+
+    if (angleRepr == "axisAngle")
+    {
+        KDL::Vector rotVector = f.M.GetRot();
+        x.resize(7);
+        x[6] = toDeg(f.M.GetRotAngle(rotVector));  // Normalizes as colateral effect
+        x[3] = rotVector[0];
+        x[4] = rotVector[1];
+        x[5] = rotVector[2];
+    }
+    else if (angleRepr == "eulerYZ") //-- like ASIBOT
+    {
+        double alfa, beta, gamma;
+        f.M.GetEulerZYZ(alfa, beta, gamma);
+        x.resize(5);
+        x[3] = toDeg(beta);
+        x[4] = toDeg(gamma);
+    }
+    else if (angleRepr == "eulerZYZ")
+    {
+        double alfa, beta, gamma;
+        f.M.GetEulerZYZ(alfa, beta, gamma);
+        x.resize(6);
+        x[3] = toDeg(alfa);
+        x[4] = toDeg(beta);
+        x[5] = toDeg(gamma);
+    }
+    else if (angleRepr == "RPY")
+    {
+        double alfa, beta, gamma;
+        f.M.GetRPY(alfa, beta, gamma);
+        x.resize(6);
+        x[3] = toDeg(alfa);
+        x[4] = toDeg(beta);
+        x[5] = toDeg(gamma);
+    }
+    else  //-- No known angle repr.
+    {
+        CD_WARNING("angleRepr unknown %s\n",angleRepr.c_str());
+    }
+
+    x[0] = f.p.data[0];
+    x[1] = f.p.data[1];
+    x[2] = f.p.data[2];
 
     return true;
 }
