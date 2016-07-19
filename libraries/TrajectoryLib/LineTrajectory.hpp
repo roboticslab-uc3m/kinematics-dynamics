cd87a775d09c557781292760c6c8b64370fa3f80
jgvictores <jcgvicto@gmail.com>
2016-07-19 14:35:00 +0200

    Trajectory and LineTrajectory files
    Former-commit-id: f6fe2d371d413cd398e94c39b1b8989c7c57326d [formerly eb48f6be35feeb2c3f3faddc81a55a74afc0cc16] [formerly 27401c67fd357b69c98298e2bd32d30b16bccb6b [formerly 27401c67fd357b69c98298e2bd32d30b16bccb6b [formerly 27401c67fd357b69c98298e2bd32d30b16bccb6b [formerly 7e71ec9f457e4d5d2b033051ce5f5af675b7b962]]]]
    Former-commit-id: 236268a372a065a83e0ce0da631e0a17afb23235 [formerly 639d4b83d811f266b3bb6548dcb42277c50470a5]
    Former-commit-id: 1c5bcc17e32cdd2f716b8f0cd5c6157fc20e700a
    Former-commit-id: 0f06999a1832b7cd972da789cea135fb5d37835a
    Former-commit-id: c206c45dffdb2aad5307a86fd62832ec295f0f52

---
+++ libraries/TrajectoryLib/LineTrajectory.hpp
@@ -0,0 +1,60 @@
+// -*- mode:C++; tab-width:4; c-basic-offset:4; indent-tabs-mode:nil -*-
+
+#ifndef __TRAJECTORY_HPP__
+#define __TRAJECTORY_HPP__
+
+#include <vector>
+
+#include <kdl/path_line.hpp>
+#include <kdl/rotational_interpolation_sa.hpp>
+#include <kdl/velocityprofile_rect.hpp>
+#include <kdl/velocityprofile_trap.hpp>
+#include <kdl/trajectory_segment.hpp>
+
+#include "ColorDebug.hpp"
+#include "KdlVectorConverter.hpp"
+
+#define DEFAULT_MAXVEL 7.5      // unit/s
+#define DEFAULT_MAXACC 0.2      // unit/s^2
+#define DEFAULT_DURATION 10
+
+namespace teo
+{
+
+/**
+ * @ingroup teo_libraries
+ * \defgroup TrajectoryLib
+ *
+ * @brief Contains classes related to trajectories.
+ */
+
+/**
+ * @ingroup TrajectoryLib
+ * @brief Implements a basic trajectory.
+ */
+
+class LineTrajectory : public KdlVectorConverter
+{
+public:
+
+    LineTrajectory();
+
+    /** Cartesian position of the trajectory at movementTime */
+    bool getX(const double movementTime, std::vector<double>& x);
+
+    /** Cartesian velocity of the trajectory at movementTime */
+    bool getXdot(const double movementTime, std::vector<double>& xdot);
+
+    bool newLine(const std::vector<double> &src, const std::vector<double> &dest);
+    bool deleteLine();
+
+private:
+
+    KDL::Trajectory_Segment* currentTrajectory;
+    KDL::RotationalInterpolation_SingleAxis* _orient;
+
+};
+
+}  // namespace teo
+
+#endif  // __TRAJECTORY_HPP__
