a271a3f8793bcaef9bfc1cd9236bfdeb4d5bb238
jgvictores <jcgvicto@gmail.com>
2016-07-26 13:39:45 +0200

    begin TwoLimbCartesianControl
    Former-commit-id: 1e44859e7a34d0d3d726ab779235a7dc41220c9b [formerly ab157674eec4af5a1cdc076da11603fe2f38dbc6] [formerly dd807fbebaed4e22d9d2d82f379c5aad675ebabf [formerly dd807fbebaed4e22d9d2d82f379c5aad675ebabf [formerly dd807fbebaed4e22d9d2d82f379c5aad675ebabf [formerly 8ae07596494e5f4a499d9c9801204c6c7b0d432e]]]]
    Former-commit-id: 8ae019a54e6cfa7fa99c67fae2cc0b8e32159413 [formerly f0323afa75bf24528208cabfa138dd3bea1d86ba]
    Former-commit-id: c6de3bda52a9c7068e53c7eb01c226dca30f05d6
    Former-commit-id: 5eb80ebd20016750a8ada3aea8a612490689e6d2
    Former-commit-id: a7bf9d38a1b62d3592fdf6ed36a88ca6754c1014

---
+++ libraries/TeoYarp/BasicTwoLimbCartesianControl/BasicTwoLimbCartesianControl.hpp
@@ -0,0 +1,139 @@
+// -*- mode:C++; tab-width:4; c-basic-offset:4; indent-tabs-mode:nil -*-
+
+#ifndef __BASIC_CARTESIAN_CONTROL_HPP__
+#define __BASIC_CARTESIAN_CONTROL_HPP__
+
+#include <yarp/os/all.h>
+#include <yarp/dev/Drivers.h>
+#include <yarp/dev/PolyDriver.h>
+#include <yarp/dev/ControlBoardInterfaces.h>
+
+#include <iostream> // only windows
+
+#include <math.h>  //-- fabs
+
+#include "ICartesianSolver.h"
+#include "ICartesianControl.h"
+
+#include "LineTrajectory.hpp"
+
+#include "ColorDebug.hpp"
+
+#define DEFAULT_SOLVER "KdlSolver"
+#define DEFAULT_ROBOT "remote_controlboard"
+#define DEFAULT_INIT_STATE VOCAB_CC_NOT_CONTROLLING
+#define DEFAULT_MS 50
+#define MAX_ANG_VEL 7.5
+#define DEFAULT_GAIN 1.0
+
+namespace teo
+{
+
+/**
+ * @ingroup TeoYarp
+ * \defgroup BasicTwoLimbCartesianControl
+ *
+ * @brief Contains teo::BasicTwoLimbCartesianControl.
+ */
+
+/**
+ * @ingroup BasicTwoLimbCartesianControl
+ * @brief The BasicTwoLimbCartesianControl class implements ICartesianSolver.
+ */
+
+class BasicTwoLimbCartesianControl : public yarp::dev::DeviceDriver, public ICartesianControl, public yarp::os::RateThread {
+
+    public:
+
+        BasicTwoLimbCartesianControl() : currentState(DEFAULT_INIT_STATE), RateThread(DEFAULT_MS) {}
+
+        // -- ICartesianControl declarations. Implementation in ICartesianControlImpl.cpp--
+        /** Inform on control state, and get robot position and perform forward kinematics. */
+        virtual bool stat(int &state, std::vector<double> &x);
+
+        /** Perform inverse kinematics (using robot position as initial guess) but do not move. */
+        virtual bool inv(const std::vector<double> &xd, std::vector<double> &q);
+
+        /** movj */
+        virtual bool movj(const std::vector<double> &xd);
+
+        /** movl */
+        virtual bool movl(const std::vector<double> &xd);
+
+        /** movv */
+        virtual bool movv(const std::vector<double> &xdotd);
+
+        /** gcmp */
+        virtual bool gcmp();
+
+        /** forc */
+        virtual bool forc(const std::vector<double> &td);
+
+        /** stop */
+        virtual bool stopControl();
+
+        // -------- RateThread declarations. Implementation in RateThreadImpl.cpp --------
+
+        /** Loop function. This is the thread itself. */
+        virtual void run();
+
+        // -------- DeviceDriver declarations. Implementation in IDeviceImpl.cpp --------
+
+        /**
+        * Open the DeviceDriver.
+        * @param config is a list of parameters for the device.
+        * Which parameters are effective for your device can vary.
+        * See \ref dev_examples "device invocation examples".
+        * If there is no example for your device,
+        * you can run the "yarpdev" program with the verbose flag
+        * set to probe what parameters the device is checking.
+        * If that fails too,
+        * you'll need to read the source code (please nag one of the
+        * yarp developers to add documentation for your device).
+        * @return true/false upon success/failure
+        */
+        virtual bool open(yarp::os::Searchable& config);
+
+        /**
+        * Close the DeviceDriver.
+        * @return true/false on success/failure.
+        */
+        virtual bool close();
+
+protected:
+
+        yarp::dev::PolyDriver solverDevice;
+        teo::ICartesianSolver *iCartesianSolver;
+
+        yarp::dev::PolyDriver robotDevice;
+        yarp::dev::IEncoders *iEncoders;
+        yarp::dev::IPositionControl *iPositionControl;
+        yarp::dev::IVelocityControl *iVelocityControl;
+        yarp::dev::IControlLimits *iControlLimits;
+        yarp::dev::ITorqueControl *iTorqueControl;
+
+        int numRobotJoints, numSolverLinks;
+
+        /** State encoded as a VOCAB which can be stored as an int */
+        int currentState;
+
+        int getCurrentState();
+        void setCurrentState(int value);
+        yarp::os::Semaphore currentStateReady;
+
+        /** MOVL keep track of movement start time to know at what time of trajectory movement we are */
+        double movementStartTime;
+
+        /** MOVL store Cartesian trajectory */
+        LineTrajectory trajectory;
+
+        /** MOVV desired Cartesian velocity */
+        std::vector<double> xdotd;
+
+        /** FORC desired Cartesian force */
+        std::vector<double> td;
+};
+
+}  // namespace teo
+
+#endif  // __BASIC_CARTESIAN_CONTROL_HPP__
---
+++ libraries/TeoYarp/TwoLimbCartesianControlServer/TwoLimbCartesianControlServer.hpp
@@ -0,0 +1,77 @@
+// -*- mode:C++; tab-width:4; c-basic-offset:4; indent-tabs-mode:nil -*-
+
+#ifndef __TWO_LIMB_CARTESIAN_CONTROL_HPP__
+#define __TWO_LIMB_CARTESIAN_CONTROL_HPP__
+
+#include <yarp/os/all.h>
+#include <yarp/dev/Drivers.h>
+#include <yarp/dev/PolyDriver.h>
+#include <yarp/dev/ControlBoardInterfaces.h>
+
+#include <iostream> // only windows
+
+#include "ITwoLimbCartesianControl.h"
+
+#include "ColorDebug.hpp"
+
+namespace teo
+{
+
+/**
+ * @ingroup TeoYarp
+ * \defgroup TwoLimbCartesianControlServer
+ *
+ * @brief Contains teo::TwoLimbCartesianControlServer.
+ */
+
+/**
+ * @ingroup TwoLimbCartesianControlServer
+ * @brief The TwoLimbCartesianControlServer class implements ICartesianSolver.
+ */
+
+class TwoLimbCartesianControlServer : public yarp::dev::DeviceDriver, public yarp::os::PortReader {
+
+    public:
+
+        TwoLimbCartesianControlServer() {}
+
+        // -------- DeviceDriver declarations. Implementation in IDeviceImpl.cpp --------
+
+        /**
+        * Open the DeviceDriver.
+        * @param config is a list of parameters for the device.
+        * Which parameters are effective for your device can vary.
+        * See \ref dev_examples "device invocation examples".
+        * If there is no example for your device,
+        * you can run the "yarpdev" program with the verbose flag
+        * set to probe what parameters the device is checking.
+        * If that fails too,
+        * you'll need to read the source code (please nag one of the
+        * yarp developers to add documentation for your device).
+        * @return true/false upon success/failure
+        */
+        virtual bool open(yarp::os::Searchable& config);
+
+        /**
+        * Close the DeviceDriver.
+        * @return true/false on success/failure.
+        */
+        virtual bool close();
+
+        // -------- PortReader declarations. Implementation in TwoLimbCartesianControlServer.cpp --------
+
+        virtual bool read(yarp::os::ConnectionReader& connection);
+
+    protected:
+
+        yarp::os::RpcServer rpcServer;
+
+        yarp::dev::PolyDriver cartesianControlDevice;
+        teo::ITwoLimbCartesianControl *iTwoLimbCartesianControl;
+
+};
+
+}  // namespace teo
+
+#endif  // __TWO_LIMB_CARTESIAN_CONTROL_HPP__
+
