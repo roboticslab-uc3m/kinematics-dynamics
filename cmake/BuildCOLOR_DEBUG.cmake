873e73e432439b297938fcb58c9e0d82261f327c
Bartek Łukawski <bwmn.peter@gmail.com>
2017-03-13 14:03:18 +0100

    Avoid hardcoding install path, use ExternalProject_Get_Property
    We shouldn't hardcode build/install/ without assuring that COLOR_DEBUG
    will be actually downloaded (might be found on the system). Instead of
    that, get the actual install path set by ycm_ep_helper() and include
    only upon download.
    
    http://stackoverflow.com/a/15176075

--- cmake/BuildCOLOR_DEBUG.cmake
+++ cmake/BuildCOLOR_DEBUG.cmake
@@ -4,3 +4,6 @@ ycm_ep_helper(COLOR_DEBUG TYPE GIT
               STYLE GITHUB
               REPOSITORY roboticslab-uc3m/color-debug.git
               TAG master)
+
+ExternalProject_Get_Property(COLOR_DEBUG INSTALL_DIR)
+include_directories(${INSTALL_DIR})
