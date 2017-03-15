496f68c1304c9d0285a525c51f66b8b64c3341e3
jgvictores <jcgvicto@gmail.com>
2017-03-15 16:29:52 +0100

    update naming in doxygen

--- programs/teoSim/main.cpp
+++ programs/teoSim/main.cpp
@@ -1,7 +1,7 @@
 // -*- mode:C++; tab-width:4; c-basic-offset:4; indent-tabs-mode:nil -*-
 
 /**
- * @ingroup teo_programs
+ * @ingroup kinematics-dynamics-programs
  *
  * \defgroup teoSim teoSim
  *
