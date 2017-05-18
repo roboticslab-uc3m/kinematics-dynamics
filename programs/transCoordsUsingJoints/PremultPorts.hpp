791b0a30692c6db1fc000f9bc925bd5e672225b2
Bartek Łukawski <bwmn.peter@gmail.com>
2017-05-18 22:01:23 +0200

    Temporary fix to avoid #97
    Fixed at robotology/yarp#1127.

--- programs/transCoordsUsingJoints/PremultPorts.hpp
+++ programs/transCoordsUsingJoints/PremultPorts.hpp
@@ -7,7 +7,20 @@
 #include <yarp/os/BufferedPort.h>
 
 #include <yarp/dev/api.h>
+
+// Temporary fix to avoid https://github.com/roboticslab-uc3m/kinematics-dynamics/issues/97
+// Affects YARP 2.3.68 and 2.3.69-2.3.69.6
+#include <yarp/conf/version.h>
+#define __YARP_VERSION_N (YARP_VERSION_MAJOR * 100000 \
+                          + YARP_VERSION_MINOR * 10000 \
+                          + YARP_VERSION_PATCH * 10)
+#if (YARP_VERSION_TWEAK + 0)  // may be empty
+#define __YARP_VERSION_N (__YARP_VERSION_N + YARP_VERSION_TWEAK)
+#endif
+#if __YARP_VERSION_N == 230680 || (__YARP_VERSION_N >= 230690 && __YARP_VERSION_N <= 230696)
 #include <yarp/dev/IEncoders.h>
+#endif
+#undef __YARP_VERSION_N
 
 #include <kdl/frames.hpp>
 #include <kdl/chain.hpp>
