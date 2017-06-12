84b158982e8fef3eb2e712a6d33844509d0e97ea
jgvictores <jcgvicto@gmail.com>
2017-06-13 00:43:09 +0200

    implement deprecated CD_WARNING and return false

--- libraries/TeoYarp/KdlSolver/ICartesianSolverImpl.cpp
+++ libraries/TeoYarp/KdlSolver/ICartesianSolverImpl.cpp
@@ -4,6 +4,13 @@
 
 // -----------------------------------------------------------------------------
 
+bool roboticslab::KdlSolver::getNumLinks(int* numLinks) {
+    CD_WARNING("getNumLinks deprecated: use getNumJoints instead.\n");
+    return false;
+}
+
+// -----------------------------------------------------------------------------
+
 bool roboticslab::KdlSolver::getNumJoints(int* numJoints) {
     *numJoints = this->chain.getNrOfJoints();
     return true;
