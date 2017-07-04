80f597327372300c5b7d846cdeea29708dbc58cf
Bartek Łukawski <bwmn.peter@gmail.com>
2017-07-05 00:38:43 +0200

    Implement invKin (no q guess)
    Imported from https://github.com/roboticslab-uc3m/asibot-main

--- libraries/TeoYarp/AsibotSolver/ICartesianSolverImpl.cpp
+++ libraries/TeoYarp/AsibotSolver/ICartesianSolverImpl.cpp
@@ -40,7 +40,7 @@ bool roboticslab::AsibotSolver::fwdKin(const std::vector<double> &q, std::vector
     double phP = A1 * std::cos(qInRad[1]) + A2 * std::cos(qInRad[1] + qInRad[2]) + A3 * std::cos(qInRad[1] + qInRad[2] + qInRad[3]);
     double oyP = q[1] + q[2] + q[3];  // [deg]
 
-    x.resize(5);
+    x.resize(5);  // translation[3] + rotation[2]
 
     x[0] = prP * std::cos(qInRad[0]);
     x[1] = prP * std::sin(qInRad[0]);
@@ -62,6 +62,35 @@ bool roboticslab::AsibotSolver::fwdKinError(const std::vector<double> &xd, const
 
 bool roboticslab::AsibotSolver::invKin(const std::vector<double> &xd, const std::vector<double> &qGuess, std::vector<double> &q)
 {
+    double ozdRad = std::atan2(xd[1], xd[0]);
+
+    double prPd = std::sqrt(xd[0] * xd[0] + xd[1] * xd[1]);
+    double phPd = xd[2] - A0;
+    double oyPd = xd[3];
+
+    double prWd = prPd - A3 * std::sin(toRad(oyPd));
+    double phWd = phPd - A3 * std::cos(toRad(oyPd));
+
+    double ct2 = (prWd * prWd + phWd * phWd - A1 * A1 - A2 * A2) / (2 * A1 * A2);
+    double st2 = std::sqrt(1 - ct2 * ct2);  // forces elbow-up in ASIBOT
+    //double st2 = -std::sqrt(1 - ct2 * ct2);  // forces elbow-down in ASIBOT
+
+    double t2Rad = std::atan2(st2, ct2);
+
+    double st1 = ((A1 + A2 * ct2) * prWd - A2 * st2 * phWd) / (phWd * phWd + prWd * prWd);
+    double ct1 = ((A1 + A2 * ct2) * phWd + A2 * st2 * prWd) / (phWd * phWd + prWd * prWd);
+    // double ct1 = (phWd + A2 * st1 * st2) / (A1 + A2 * ct2);  // alternative method for same result
+
+    double t1Rad = std::atan2(st1, ct1);
+
+    q.resize(NUM_MOTORS);
+
+    q[0] = toDeg(ozdRad);
+    q[1] = toDeg(t1Rad);
+    q[2] = toDeg(t2Rad);
+    q[3] = oyPd - q[1] - q[2];
+    q[4] = xd[4];  // ozPP
+
     return true;
 }
 
