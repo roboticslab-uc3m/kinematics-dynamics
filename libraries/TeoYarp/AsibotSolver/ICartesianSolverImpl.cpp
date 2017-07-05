cef232a0860cdc45228368b2f7139a069f1be891
Bartek Łukawski <bwmn.peter@gmail.com>
2017-07-05 18:23:21 +0200

    Implement diffInvKin, improve legibility of trigonometric functions

--- libraries/TeoYarp/AsibotSolver/ICartesianSolverImpl.cpp
+++ libraries/TeoYarp/AsibotSolver/ICartesianSolverImpl.cpp
@@ -35,15 +35,26 @@ bool roboticslab::AsibotSolver::fwdKin(const std::vector<double> &q, std::vector
         *it = toRad(*it);
     }
 
+    double s1 = std::sin(qInRad[0]);
+    double c1 = std::cos(qInRad[0]);
+    double s2 = std::sin(qInRad[1]);
+    double c2 = std::cos(qInRad[1]);
+
+    double s23 = std::sin(qInRad[1] + qInRad[2]);
+    double c23 = std::cos(qInRad[1] + qInRad[2]);
+
+    double s234 = std::sin(qInRad[1] + qInRad[2] + qInRad[3]);
+    double c234 = std::cos(qInRad[1] + qInRad[2] + qInRad[3]);
+
     // P = prime
-    double prP = A1 * std::sin(qInRad[1]) + A2 * std::sin(qInRad[1] + qInRad[2]) + A3 * std::sin(qInRad[1] + qInRad[2] + qInRad[3]);
-    double phP = A1 * std::cos(qInRad[1]) + A2 * std::cos(qInRad[1] + qInRad[2]) + A3 * std::cos(qInRad[1] + qInRad[2] + qInRad[3]);
+    double prP = A1 * s2 + A2 * s23 + A3 * s234;
+    double phP = A1 * c2 + A2 * c23 + A3 * c234;
     double oyP = q[1] + q[2] + q[3];  // [deg]
 
     x.resize(5);  // translation[3] + rotation[2]
 
-    x[0] = prP * std::cos(qInRad[0]);
-    x[1] = prP * std::sin(qInRad[0]);
+    x[0] = prP * c1;
+    x[1] = prP * s1;
     x[2] = phP + A0;  // pz = pzP
     x[3] = oyP;  // = pitchP
     x[4] = q[4];  // = ozPP
@@ -109,6 +120,76 @@ bool roboticslab::AsibotSolver::invKin(const std::vector<double> &xd, const std:
 
 bool roboticslab::AsibotSolver::diffInvKin(const std::vector<double> &q, const std::vector<double> &xdot, std::vector<double> &qdot)
 {
+    std::vector<double> qInRad(q);
+
+    for (std::vector<double>::iterator it = qInRad.begin(); it != qInRad.end(); ++it)
+    {
+        *it = toRad(*it);
+    }
+
+    double s1 = std::sin(qInRad[0]);
+    double c1 = std::cos(qInRad[0]);
+
+    double s2 = std::sin(qInRad[1]);
+    double c2 = std::cos(qInRad[1]);
+
+    double s23 = std::sin(qInRad[1] + qInRad[2]);
+    double c23 = std::cos(qInRad[1] + qInRad[2]);
+
+    double s234 = std::sin(qInRad[1] + qInRad[2] + qInRad[3]);
+    double c234 = std::cos(qInRad[1] + qInRad[2] + qInRad[3]);
+
+    yarp::sig::Matrix Ja(5, 5);
+
+    Ja(0, 0) = -s1 * (A3 * s234 + A2 * s23 + A1 * s2);
+    Ja(0, 1) =  c1 * (A3 * c234 + A2 * c23 + A1 * c2);
+    Ja(0, 2) =  c1 * (A3 * c234 + A2 * c23);
+    Ja(0, 3) =  c1 * A3 * c234;
+    Ja(0, 4) =  0;
+
+    Ja(1, 0) = c1 * (A3 * s234 + A2 * s23 + A1 * s2);
+    Ja(1, 1) = s1 * (A3 * c234 + A2 * c23 + A1 * c2);
+    Ja(1, 2) = s1 * (A3 * c234 + A2 * c23);
+    Ja(1, 3) = s1 * A3 * c234;
+    Ja(1, 4) = 0;
+
+    Ja(2, 0) = 0;
+    Ja(2, 1) = -A3 * s234 - A2 * s23 - A1 * s2;
+    Ja(2, 2) = -A3 * s234 - A2 * s23;
+    Ja(2, 3) = -A3 * s234;
+    Ja(2, 4) = 0;
+
+    Ja(3, 0) = 0;
+    Ja(3, 1) = 1;
+    Ja(3, 2) = 1;
+    Ja(3, 3) = 1;
+    Ja(3, 4) = 0;
+
+    Ja(4, 0) = 0;
+    Ja(4, 1) = 0;
+    Ja(4, 2) = 0;
+    Ja(4, 3) = 0;
+    Ja(4, 4) = 1;
+
+    yarp::sig::Matrix Ja_inv = yarp::math::luinv(Ja);
+    yarp::sig::Vector xdotv(5, 0.0);
+
+    for (int i = 0; i < xdot.size(); i++)
+    {
+        xdotv[i] = xdot[i];
+    }
+
+    using namespace yarp::math;
+    yarp::sig::Vector qdotv = Ja_inv * xdotv;
+
+    qdot.resize(NUM_MOTORS);
+
+    qdot[0] = toDeg(qdotv[0]);
+    qdot[1] = toDeg(qdotv[1]);
+    qdot[2] = toDeg(qdotv[2]);
+    qdot[3] = toDeg(qdotv[3]);
+    qdot[4] = toDeg(qdotv[4]);
+
     return true;
 }
 
