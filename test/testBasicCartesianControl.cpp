231d1376c7078fc3dc55742e51e41c73f9b4f50e
Bartek Łukawski <bwmn.peter@gmail.com>
2017-07-17 16:22:38 +0200

    Review CMake code and #include's for test programs

--- test/testBasicCartesianControl.cpp
+++ test/testBasicCartesianControl.cpp
@@ -1,14 +1,14 @@
 #include "gtest/gtest.h"
 
+#include <vector>
+
 #include <yarp/os/all.h>
 #include <yarp/dev/Drivers.h>
 #include <yarp/dev/PolyDriver.h>
 
+#include <ColorDebug.hpp>
+
 #include "ICartesianControl.h"
-
-#include "ColorDebug.hpp"
-
-//YARP_DECLARE_PLUGINS(roboticslabYarp)
 
 namespace roboticslab
 {
@@ -21,8 +21,6 @@ class BasicCartesianControlTest : public testing::Test
 
     public:
         virtual void SetUp() {
-            //YARP_REGISTER_PLUGINS(roboticslabYarp);
-
             yarp::os::Property cartesianControlOptions("(device BasicCartesianControl) (robot FakeControlboard) (axes 1) (solver KdlSolver) (angleRepr axisAngle) (gravity 0 -10 0) (numLinks 1) (link_0 (A 1) (mass 1) (cog -0.5 0 0) (inertia 1 1 1))");
 
             cartesianControlDevice.open(cartesianControlOptions);
