08c26d876f476c8b101f752c21654739f3656c1e
Bartek Łukawski <bwmn.peter@gmail.com>
2017-07-22 11:05:58 +0200

    Add more IK unit tests

--- test/testAsibotSolverFromFile.cpp
+++ test/testAsibotSolverFromFile.cpp
@@ -108,6 +108,7 @@ TEST_F(AsibotSolverTestFromFile, AsibotSolverInvKin1)
     xd[3] = 45.0;  //-- oyP
     xd[4] = 0.0;  //-- ozPP
 
+    // forces FORWARD UP, compare with AsibotSolverSetLimits
     qGuess[2] = 90.0;
 
     ASSERT_TRUE(iCartesianSolver->invKin(xd, qGuess, q));
@@ -118,6 +119,138 @@ TEST_F(AsibotSolverTestFromFile, AsibotSolverInvKin1)
     ASSERT_NEAR(q[1], 0.0, 1e-3);
     ASSERT_NEAR(q[2], 45.0, 1e-3);
     ASSERT_NEAR(q[3], 0.0, 1e-3);
+    ASSERT_NEAR(q[4], 0.0, 1e-3);
+}
+
+TEST_F(AsibotSolverTestFromFile, AsibotSolverInvKin2)
+{
+    std::vector<double> xd(5), qGuess(5, 0.0), q;
+
+    xd[0] = -0.777817459;  //-- x
+    xd[1] = 0.0;  //-- y
+    xd[2] = 1.077817459;  //-- z
+    xd[3] = 45.0;  //-- oyP
+    xd[4] = 180.0;  //-- ozPP
+
+    // force REVERSED
+    qGuess[1] = 45.0;
+
+    ASSERT_TRUE(iCartesianSolver->invKin(xd, qGuess, q));
+
+    ASSERT_EQ(q.size(), 5);  //-- eulerYZ
+
+    // increasing eps at q2-4
+    ASSERT_NEAR(q[0], 0.0, 1e-3);
+    ASSERT_NEAR(q[1], -45.0, 1e-2);
+    ASSERT_NEAR(q[2], 0.0, 1e-2);
+    ASSERT_NEAR(q[3], 0.0, 1e-2);
+    ASSERT_NEAR(q[4], 0.0, 1e-3);
+}
+
+TEST_F(AsibotSolverTestFromFile, AsibotSolverInvKin3)
+{
+    std::vector<double> xd(5), qGuess(5, 0.0), q;
+
+    xd[0] = -0.318163634;  //-- x
+    xd[1] = 0.379172654;  //-- y
+    xd[2] = 1.194974747;  //-- z
+    xd[3] = 45.0;  //-- oyP
+    xd[4] = 0.0;  //-- ozPP
+
+    // force FORWARD
+    qGuess[1] = 45.0;
+
+    ASSERT_TRUE(iCartesianSolver->invKin(xd, qGuess, q));
+
+    ASSERT_EQ(q.size(), 5);  //-- eulerYZ
+
+    ASSERT_NEAR(q[0], 130.0, 1e-3);
+    ASSERT_NEAR(q[1], 0.0, 1e-3);
+    ASSERT_NEAR(q[2], 45.0, 1e-3);
+    ASSERT_NEAR(q[3], 0.0, 1e-3);
+    ASSERT_NEAR(q[4], 0.0, 1e-3);
+
+    // force REVERSED
+    xd[4] = 180.0;
+
+    ASSERT_TRUE(iCartesianSolver->invKin(xd, qGuess, q));
+
+    ASSERT_NEAR(q[0], -50.0, 1e-3);
+    ASSERT_NEAR(q[1], 0.0, 1e-3);
+    ASSERT_NEAR(q[2], -45.0, 1e-3);
+    ASSERT_NEAR(q[3], 0.0, 1e-3);
+    ASSERT_NEAR(q[4], 0.0, 1e-3);
+}
+
+TEST_F(AsibotSolverTestFromFile, AsibotSolverInvKin4)
+{
+    std::vector<double> xd(5), qGuess(5, 0.0), q;
+
+    xd[0] = -0.379172654;  //-- x
+    xd[1] = 0.318163634;  //-- y
+    xd[2] = 1.194974747;  //-- z
+    xd[3] = 45.0;  //-- oyP
+    xd[4] = 180.0;  //-- ozPP
+
+    // force REVERSED
+    qGuess[1] = 45.0;
+
+    ASSERT_TRUE(iCartesianSolver->invKin(xd, qGuess, q));
+
+    ASSERT_EQ(q.size(), 5);  //-- eulerYZ
+
+    ASSERT_NEAR(q[0], -40.0, 1e-3);
+    ASSERT_NEAR(q[1], 0.0, 1e-3);
+    ASSERT_NEAR(q[2], -45.0, 1e-3);
+    ASSERT_NEAR(q[3], 0.0, 1e-3);
+    ASSERT_NEAR(q[4], 0.0, 1e-3);
+
+    // force FORWARD
+    xd[4] = 0.0;
+
+    ASSERT_TRUE(iCartesianSolver->invKin(xd, qGuess, q));
+
+    ASSERT_NEAR(q[0], 140.0, 1e-3);
+    ASSERT_NEAR(q[1], 0.0, 1e-3);
+    ASSERT_NEAR(q[2], 45.0, 1e-3);
+    ASSERT_NEAR(q[3], 0.0, 1e-3);
+    ASSERT_NEAR(q[4], 0.0, 1e-3);
+}
+
+TEST_F(AsibotSolverTestFromFile, AsibotSolverInvKin5)
+{
+    std::vector<double> xd(5), qGuess(5, 0.0), q;
+
+    xd[0] = -0.4;  //-- x
+    xd[1] = 0.0;  //-- y
+    xd[2] = 1.1;  //-- z
+    xd[3] = 26.56505118;  //-- oyP
+    xd[4] = 180.0;  //-- ozPP
+
+    // force REVERSED UP
+    qGuess[1] = 15.0;
+    qGuess[2] = 30.0;
+
+    ASSERT_TRUE(iCartesianSolver->invKin(xd, qGuess, q));
+
+    ASSERT_EQ(q.size(), 5);  //-- eulerYZ
+
+    ASSERT_NEAR(q[0], 0.0, 1e-3);
+    ASSERT_NEAR(q[1], 15.44443859, 1e-3);
+    ASSERT_NEAR(q[2], -84.01897954, 1e-3);
+    ASSERT_NEAR(q[3], 42.00948977, 1e-3);
+    ASSERT_NEAR(q[4], 0.0, 1e-3);
+
+    // force REVERSED DOWN
+    qGuess[1] = -30.0;
+    qGuess[2] = 90.0;
+
+    ASSERT_TRUE(iCartesianSolver->invKin(xd, qGuess, q));
+
+    ASSERT_NEAR(q[0], 0.0, 1e-3);
+    ASSERT_NEAR(q[1], -68.57454095, 1e-3);
+    ASSERT_NEAR(q[2], 84.01897954, 1e-3);
+    ASSERT_NEAR(q[3], -42.00948977, 1e-3);
     ASSERT_NEAR(q[4], 0.0, 1e-3);
 }
 
