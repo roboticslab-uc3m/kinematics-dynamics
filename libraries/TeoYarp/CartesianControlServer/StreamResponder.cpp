8d7ec098197d58f2bb3b2caae771a8558b06e952
Bartek Łukawski <bwmn.peter@gmail.com>
2017-07-26 19:55:54 +0200

    Listen to vmos commands at StreamResponder

--- libraries/TeoYarp/CartesianControlServer/StreamResponder.cpp
+++ libraries/TeoYarp/CartesianControlServer/StreamResponder.cpp
@@ -8,7 +8,36 @@
 
 void roboticslab::StreamResponder::onRead(yarp::os::Bottle& b)
 {
+    CD_DEBUG("Got: %s\n", b.toString().c_str());
+    CD_DEBUG("Size: %d\n", b.size());
+    switch (b.get(0).asVocab())
+    {
+    case VOCAB_CC_VMOS:
+        {
+            if (b.size() > 1)
+            {
+                std::vector<double> v;
 
+                for (size_t i = 1; i < b.size(); i++)
+                {
+                    v.push_back(b.get(i).asDouble());
+                }
+
+                if (!iCartesianControl->vmos(v))
+                {
+                    CD_ERROR("vmos failed\n");
+                }
+            }
+            else
+            {
+                CD_ERROR("size error\n");
+            }
+        }
+        break;
+    default:
+        CD_ERROR("command not recognized\n");
+        break;
+    }
 }
 
 // -----------------------------------------------------------------------------
