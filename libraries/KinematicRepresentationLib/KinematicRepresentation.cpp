d15d7efc5bdf27143bbfb4ae990ab8ff5426cce3
Bartek Łukawski <bwmn.peter@gmail.com>
2017-08-17 17:39:31 +0200

    Prepare directory structure for new KinematicRepresentationLib library

---
+++ libraries/KinematicRepresentationLib/KinematicRepresentation.cpp
@@ -0,0 +1,7 @@
+// -*- mode:C++; tab-width:4; c-basic-offset:4; indent-tabs-mode:nil -*-
+
+#include "KinematicRepresentation.hpp"
+
+#include <ColorDebug.hpp>
+
+// -----------------------------------------------------------------------------
---
+++ libraries/KinematicRepresentationLib/KinematicRepresentation.hpp
@@ -0,0 +1,40 @@
+// -*- mode:C++; tab-width:4; c-basic-offset:4; indent-tabs-mode:nil -*-
+
+#ifndef __KINEMATIC_REPRESENTATION_HPP__
+#define __KINEMATIC_REPRESENTATION_HPP__
+
+namespace roboticslab
+{
+
+/**
+ * @ingroup kinematics-dynamics-libraries
+ * @defgroup KinematicRepresentationLib
+ *
+ * @brief Contains classes related to conversion mechanisms
+ * between different coordinate and orientation systems.
+ */
+
+/**
+ * @ingroup KinematicRepresentationLib
+ * @brief Stores pose values.
+ */
+class KinPose
+{};
+
+/**
+ * @ingroup KinematicRepresentationLib
+ * @brief Stores velocity values.
+ */
+class KinVelocity
+{};
+
+/**
+ * @ingroup KinematicRepresentationLib
+ * @brief Stores acceleration values.
+ */
+class KinAcceleration
+{};
+
+}  // namespace roboticslab
+
+#endif  // __KINEMATIC_REPRESENTATION_HPP__
