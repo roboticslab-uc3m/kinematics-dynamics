423d35650e9acfc305a1a010ed0433a5907ef805
Bartek Łukawski <bwmn.peter@gmail.com>
2017-09-07 18:02:21 +0200

    Use KinRepresentation::parseEnumerator in CCS upon device configuration

--- libraries/TeoYarp/CartesianControlServer/DeviceDriverImpl.cpp
+++ libraries/TeoYarp/CartesianControlServer/DeviceDriverImpl.cpp
@@ -73,23 +73,7 @@ bool roboticslab::CartesianControlServer::open(yarp::os::Searchable& config)
         std::string angleReprStr = angleRepr->asString();
         KinRepresentation::orientation_system orient;
 
-        if (angleReprStr == "axisAngle")
-        {
-            orient = KinRepresentation::AXIS_ANGLE;
-        }
-        else if (angleReprStr == "eulerYZ")
-        {
-            orient = KinRepresentation::EULER_YZ;
-        }
-        else if (angleReprStr == "eulerZYZ")
-        {
-            orient = KinRepresentation::EULER_ZYZ;
-        }
-        else if (angleReprStr == "RPY")
-        {
-            orient = KinRepresentation::RPY;
-        }
-        else
+        if (!KinRepresentation::parseEnumerator(angleReprStr, &orient))
         {
             CD_WARNING("Unknown angleRepr \"%s\".\n", angleReprStr.c_str());
             return true;
