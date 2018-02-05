a4a24c13bfa091558e37bda4262f588bdbf77e61
Bartek Łukawski <bwmn.peter@gmail.com>
2018-02-05 14:52:31 +0100

    Detect version of GTestSources package

--- cmake/FindGTestSources.cmake
+++ cmake/FindGTestSources.cmake
@@ -5,6 +5,7 @@
 # GTestSources_FOUND       - system has GTest sources
 # GTestSources_SOURCE_DIR  - GTest source dir (with CMakeLists.txt)
 # GTestSources_INCLUDE_DIR - GTest include directory (public headers)
+# GTestSources_VERSION     - GTest version (if supported)
 #
 # You can set the GTEST_ROOT environment variable to be used as a
 # hint by FindGTestSources to locate googletest source directory.
@@ -27,9 +28,44 @@ if(NOT GTestSources_INCLUDE_DIR)
                                              ${GTEST_ROOT}/include)
 endif()
 
+set(_cmake_include_dirs ${CMAKE_REQUIRED_INCLUDES})
+
+include(CheckCXXSourceCompiles)
+list(APPEND CMAKE_REQUIRED_INCLUDES ${GTestSources_INCLUDE_DIR})
+
+check_cxx_source_compiles("
+        #include <gtest/gtest.h>
+        int main() {
+            typedef const char* (testing::TestInfo::*fun)() const;
+            fun f = &testing::TestInfo::type_param;
+            return 0;
+        }"
+    _gtest_compatible_1_6_0)
+
+check_cxx_source_compiles("
+        #include <gtest/gtest.h>
+        int main() {
+            typedef bool (testing::TestInfo::*fun)() const;
+            fun f = &testing::TestInfo::is_reportable;
+            return 0;
+        }"
+    _gtest_compatible_1_7_0)
+
+if(_gtest_compatible_1_7_0)
+    set(GTestSources_VERSION 1.7.0)
+elseif(_gtest_compatible_1_6_0)
+    set(GTestSources_VERSION 1.6.0)
+else()
+    message(STATUS "FindGTestSources.cmake reports unhandled GTest version (<1.6.0)")
+    set(GTestSources_VERSION GTestSources_VERSION-NOT_FOUND)
+endif()
+
+set(CMAKE_REQUIRED_INCLUDES "${_cmake_include_dirs}")
+
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(GTestSources REQUIRED_VARS GTestSources_SOURCE_DIR
-                                                             GTestSources_INCLUDE_DIR)
+                                                             GTestSources_INCLUDE_DIR
+                                               VERSION_VAR GTestSources_VERSION)
 
 mark_as_advanced(GTestSources_SOURCE_DIR
                  GTestSources_INCLUDE_DIR)
