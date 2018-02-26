8205beea69942a14bad1bcfa08e57ebc34439b99
Bartek Łukawski <bwmn.peter@gmail.com>
2018-02-26 14:13:27 +0100

    Remove obsolete 'help' option

--- libraries/YarpPlugins/AsibotSolver/DeviceDriverImpl.cpp
+++ libraries/YarpPlugins/AsibotSolver/DeviceDriverImpl.cpp
@@ -21,20 +21,6 @@ bool roboticslab::AsibotSolver::open(yarp::os::Searchable& config)
 {
     CD_DEBUG("config: %s.\n", config.toString().c_str());
 
-    std::printf("--------------------------------------------------------------\n");
-
-    if (config.check("help"))
-    {
-        std::printf("AsibotSolver options:\n");
-        std::printf("\t--help (this help)\n");
-        std::printf("\t--A0 [m] (dist from base to motor 2, default: \"%f\")\n", DEFAULT_A0);
-        std::printf("\t--A1 [m] (dist from motor 2 to motor 3, default: \"%f\")\n", DEFAULT_A1);
-        std::printf("\t--A2 [m] (dist from motor 3 to motor 4, default: \"%f\")\n", DEFAULT_A2);
-        std::printf("\t--A3 [m] (dist from motor 4 to end-effector, default: \"%f\")\n", DEFAULT_A3);
-        std::printf("\t--invKinStrategy (IK configuration strategy, default: \"%s\")\n", DEFAULT_STRATEGY);
-        // Do not exit: let last layer exit so we get help from the complete chain.
-    }
-
     A0 = config.check("A0", yarp::os::Value(DEFAULT_A0), "length of link 1 (meters)").asDouble();
     A1 = config.check("A1", yarp::os::Value(DEFAULT_A1), "length of link 2 (meters)").asDouble();
     A2 = config.check("A2", yarp::os::Value(DEFAULT_A2), "length of link 3 (meters)").asDouble();
@@ -84,11 +70,6 @@ bool roboticslab::AsibotSolver::open(yarp::os::Searchable& config)
     {
         CD_ERROR("Unsupported IK configuration strategy: %s.\n", strategy.c_str());
         return false;
-    }
-
-    if (config.check("help"))
-    {
-        std::exit(0);
     }
 
     tcpFrameStruct.hasFrame = false;
