84e3f5dfa02350daaf370170c100f2335c92ce7e
rsantos88 <rasantos@it.uc3m.es>
2018-03-06 13:16:46 +0100

    improved application with wait function and the same movements as cpp example

--- examples/python/exampleCartesianControlClient.py
+++ examples/python/exampleCartesianControlClient.py
@@ -27,24 +27,34 @@ x = yarp.DVector()
 stat = cartesianControl.stat(x)
 print '<',yarp.Vocab.decode(stat),'[%s]' % ', '.join(map(str, x))
 
-xd = [0.389496, -0.34692, 0.16769, 1.0, 0.0, 0.0, 20.0]
+xd = [0,0,0,0,0,0,0]
 
-print '> inv [%s]' % ', '.join(map(str, xd))
-xd_vector = yarp.DVector(xd)
-qd_vector = yarp.DVector()
-if cartesianControl.inv(xd_vector,qd_vector):
-    print '< [%s]' % ', '.join(map(str, qd_vector))
-else:
-    print '< [fail]'
+xd[0] = [0.389496, -0.34692, 0.16769, 1.0, 0.0, 0.0, 0.0]
+xd[1] = [0.5, -0.34692, 0.16769, 1.0, 0.0, 0.0, 0.0]
+xd[2] = [0.5, -0.4, 0.16769, 1.0, 0.0, 0.0, 0.0]
+xd[3] = [0.5, -0.4, 0.16769, 0.0, 1.0, 0.0, -12.0]
+xd[4] = [0.5, -0.4, 0.16769, 1.0, 0.0, 0.0, -50.0]
+xd[5] = [0.389496, -0.34692, 0.16769, 1.0, 0.0, 0.0, 0.0]
+xd[6] = [3.25149848407618e-17, -0.34692, -0.221806, 1.53080849893419e-16, 1.0, -3.06161699786838e-17, 90.0]
 
-print '> movj [%s]' % ', '.join(map(str, xd))
-xd_vector = yarp.DVector(xd)
-if cartesianControl.movj(xd_vector):
-    print '< [ok]'
-else:
-    print '< [fail]'
-
-print 'delay(1)'
-yarp.Time.delay(1)
+for i in range(len(xd)):
+    print '-- movement '+str(i)+':'
+    print '> inv [%s]' % ', '.join(map(str, xd[i]))
+    xd_vector = yarp.DVector(xd[i])
+    qd_vector = yarp.DVector()
+    if cartesianControl.inv(xd_vector,qd_vector):
+        print '< [%s]' % ', '.join(map(str, qd_vector))
+    else:
+        print '< [fail]'
+    
+    print '> movj [%s]' % ', '.join(map(str, xd[i]))
+    xd_vector = yarp.DVector(xd[i])
+    if cartesianControl.movj(xd_vector):
+        print '< [ok]'
+    else:
+        print '< [fail]'
+    
+    print '< [wait...]'
+    cartesianControl.wait()
 
 print 'bye!'
