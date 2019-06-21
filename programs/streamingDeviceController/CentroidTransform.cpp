82cd847f6a299bae225a19142e2cfd9c9bbea55c
Bartek Łukawski <bwmn.peter@gmail.com>
2019-06-21 17:40:15 +0200

    Obtain rotation towards target

--- programs/streamingDeviceController/CentroidTransform.cpp
+++ programs/streamingDeviceController/CentroidTransform.cpp
@@ -20,8 +20,8 @@ bool CentroidTransform::processBottle(const yarp::os::Bottle & b)
     }
 
     // object centroids scaled to fit into [-1, 1] range
-    double cx = b.get(0).asFloat64(); // points right
-    double cy = b.get(1).asFloat64(); // points down
+    double cx = b.get(0).asFloat64(); // points right (same as AMOR TCP's x axis)
+    double cy = b.get(1).asFloat64(); // points down (same as AMOR TCP's y axis)
 
     std::vector<double> x;
 
@@ -33,23 +33,32 @@ bool CentroidTransform::processBottle(const yarp::os::Bottle & b)
 
     KDL::Frame H_base_tcp = KdlVectorConverter::vectorToFrame(x);
 
-    // express Z axis (TCP frame) in base frame
+    // express TCP's z axis in base frame
     KDL::Vector v_base = H_base_tcp.M.Inverse() * KDL::Vector(0, 0, 1);
     v_base.Normalize();
 
-    KDL::Frame H_d = KdlVectorConverter::vectorToFrame(streamingDevice->data);
+    KDL::Frame H_base_target = KdlVectorConverter::vectorToFrame(streamingDevice->data);
 
-    double norm = KDL::dot(H_d.p, v_base);
+    double norm = KDL::dot(H_base_target.p, v_base);
 
     if (norm <= 0.0)
     {
+        // no action if we move away from the target (negative TCP's z axis)
         return true;
     }
 
-    // project desired vector into TCP's Z axis, refer result to base frame
-    H_d.p = v_base * norm;
+    // project target vector into TCP's z axis, refer result to base frame
+    H_base_target.p = v_base * norm;
 
-    streamingDevice->data = KdlVectorConverter::frameToVector(H_d);
+    // find axis along which to rotate (in TCP frame) given pixel coords
+    KDL::Vector coords(cx, cy, 0);
+    KDL::Vector tcp_axis = KDL::Rotation::RotZ(KDL::PI / 2) * coords;
+    KDL::Rotation rot_tcp_target = KDL::Rotation::Rot(tcp_axis, coords.Norm());
+
+    // rotate towards the target in base frame
+    H_base_target.M = H_base_tcp.M * rot_tcp_target;
+
+    streamingDevice->data = KdlVectorConverter::frameToVector(H_base_target);
 
     return true;
 }
