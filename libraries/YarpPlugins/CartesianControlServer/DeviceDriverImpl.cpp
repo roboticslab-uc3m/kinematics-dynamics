roboticslab-uc3m/kinematics-dynamics

commit d6ca7c19983cc38c79be4d4ad75ec3ee8e0ba29c

Create transform port for custom coordinate_system, too

libraries/YarpPlugins/CartesianControlServer/DeviceDriverImpl.cpp
@@ -13,7 +13,7 @@
 
 bool roboticslab::CartesianControlServer::open(yarp::os::Searchable& config)
 {
-    yarp::os::Value *name, *angleRepr;
+    yarp::os::Value *name, *angleRepr, *coordRepr;
 
     if (config.check("subdevice", name))
     {
@@ -81,11 +81,23 @@ bool roboticslab::CartesianControlServer::open(yarp::os::Searchable& config)
         fkStreamEnabled = false;
     }
 
+    bool hasCoordRepr = config.check("coordRepr", coordRepr, "coordinate representation for transform port");
+    bool hasAngleRepr = config.check("angleRepr", angleRepr, "angle representation for transform port");
+
     // check angle representation, leave this block last to allow inner return instruction
-    if (config.check("angleRepr", angleRepr, "angle representation for transform port"))
+    if (hasCoordRepr || hasAngleRepr)
     {
+        std::string coordReprStr = coordRepr->asString();
         std::string angleReprStr = angleRepr->asString();
+
+        KinRepresentation::coordinate_system coord;
         KinRepresentation::orientation_system orient;
+
+        if (!KinRepresentation::parseEnumerator(coordReprStr, &coord))
+        {
+            CD_WARNING("Unknown coordRepr \"%s\", falling back to default.\n", coordReprStr.c_str());
+            return true;
+        }
 
         if (!KinRepresentation::parseEnumerator(angleReprStr, &orient))
         {
@@ -93,7 +105,7 @@ bool roboticslab::CartesianControlServer::open(yarp::os::Searchable& config)
             return true;
         }
 
-        rpcTransformResponder = new RpcTransformResponder(iCartesianControl, orient);
+        rpcTransformResponder = new RpcTransformResponder(iCartesianControl, coord, orient);
 
         rpcTransformServer.open(prefix + "/rpc_transform:s");
         rpcTransformServer.setReader(*rpcTransformResponder);

libraries/KinematicRepresentationLib/KinematicRepresentation.cpp
@@ -457,6 +457,31 @@ double radToDeg(double rad)
 
 // -----------------------------------------------------------------------------
 
+bool parseEnumerator(const std::string & str, coordinate_system * coord, coordinate_system fallback)
+{
+    if (str == "cartesian")
+    {
+        *coord = CARTESIAN;
+    }
+    else if (str == "cylindrical")
+    {
+        *coord = CYLINDRICAL;
+    }
+    else if (str == "spherical")
+    {
+        *coord = SPHERICAL;
+    }
+    else
+    {
+        *coord = fallback;
+        return false;
+    }
+
+    return true;
+}
+
+// -----------------------------------------------------------------------------
+
 bool parseEnumerator(const std::string & str, orientation_system * orient, orientation_system fallback)
 {
     if (str == "axisAngle")

libraries/KinematicRepresentationLib/KinematicRepresentation.hpp
@@ -177,6 +177,23 @@ double radToDeg(double rad);
  *
  * Input string    | Enum value
  * --------------- | ----------
+ * cartesian       | @ref CARTESIAN
+ * cylindrical     | @ref CYLINDRICAL
+ * spherical       | @ref SPHERICAL
+ *
+ * @param str Input string.
+ * @param coord See @ref coordinate_system.
+ * @param fallback Default value if no match found.
+ *
+ * @return true if match found, false otherwise
+ */
+bool parseEnumerator(const std::string & str, coordinate_system * coord, coordinate_system fallback = CARTESIAN);
+
+/**
+ * @brief Parses input string, returns matching enumerator value
+ *
+ * Input string    | Enum value
+ * --------------- | ----------
  * axisAngle       | @ref AXIS_ANGLE
  * axisAngleScaled | @ref AXIS_ANGLE_SCALED
  * RPY             | @ref RPY

libraries/YarpPlugins/CartesianControlServer/CartesianControlServer.hpp
@@ -163,7 +163,8 @@ class RpcTransformResponder : public RpcResponder
 {
 public:
 
-    RpcTransformResponder(roboticslab::ICartesianControl * iCartesianControl, KinRepresentation::orientation_system orient)
+    RpcTransformResponder(roboticslab::ICartesianControl * iCartesianControl, KinRepresentation::coordinate_system coord,
+            KinRepresentation::orientation_system orient)
         : RpcResponder(iCartesianControl),
           orient(orient)
     {}
@@ -173,6 +174,7 @@ protected:
     virtual bool transformIncomingData(std::vector<double>& vin);
     virtual bool transformOutgoingData(std::vector<double>& vout);
 
+    KinRepresentation::coordinate_system coord;
     KinRepresentation::orientation_system orient;
 };
 

libraries/YarpPlugins/CartesianControlServer/RpcResponder.cpp
@@ -489,14 +489,14 @@ bool roboticslab::RpcResponder::handleParameterGetterGroup(const yarp::os::Bottl
 
 bool roboticslab::RpcTransformResponder::transformIncomingData(std::vector<double>& vin)
 {
-    return KinRepresentation::encodePose(vin, vin, KinRepresentation::CARTESIAN, orient, KinRepresentation::DEGREES);
+    return KinRepresentation::encodePose(vin, vin, coord, orient, KinRepresentation::DEGREES);
 }
 
 // -----------------------------------------------------------------------------
 
 bool roboticslab::RpcTransformResponder::transformOutgoingData(std::vector<double>& vout)
 {
-    return KinRepresentation::decodePose(vout, vout, KinRepresentation::CARTESIAN, orient, KinRepresentation::DEGREES);
+    return KinRepresentation::decodePose(vout, vout, coord, orient, KinRepresentation::DEGREES);
 }
 
 // -----------------------------------------------------------------------------

commit 73c2f48179bab3ea06bb7ec83c85c0d6cf6e263f

Account for missing transform options to avoid dereferencing nullptr
Regression caused by d6ca7c1 and 3f0b861. Fixes #184.

libraries/YarpPlugins/CartesianControlServer/DeviceDriverImpl.cpp
@@ -13,7 +13,7 @@
 
 bool roboticslab::CartesianControlServer::open(yarp::os::Searchable& config)
 {
-    yarp::os::Value *name, *angleRepr, *coordRepr, *angularUnits;
+    yarp::os::Value * name;
 
     if (config.check("subdevice", name))
     {
@@ -61,8 +61,10 @@ bool roboticslab::CartesianControlServer::open(yarp::os::Searchable& config)
 
     std::string prefix = config.check("name", yarp::os::Value(DEFAULT_PREFIX), "local port prefix").asString();
 
-    rpcServer.open(prefix + "/rpc:s");
-    commandPort.open(prefix + "/command:i");
+    bool ok = true;
+
+    ok &= rpcServer.open(prefix + "/rpc:s");
+    ok &= commandPort.open(prefix + "/command:i");
 
     rpcServer.setReader(*rpcResponder);
     commandPort.useCallback(*streamResponder);
@@ -71,7 +73,7 @@ bool roboticslab::CartesianControlServer::open(yarp::os::Searchable& config)
 
     if (periodInMs > 0)
     {
-        fkOutPort.open(prefix + "/state:o");
+        ok &= fkOutPort.open(prefix + "/state:o");
 
         yarp::os::PeriodicThread::setPeriod(periodInMs * 0.001);
         yarp::os::PeriodicThread::start();
@@ -81,46 +83,66 @@ bool roboticslab::CartesianControlServer::open(yarp::os::Searchable& config)
         fkStreamEnabled = false;
     }
 
-    bool hasCoordRepr = config.check("coordRepr", coordRepr, "coordinate representation for transform port");
-    bool hasAngleRepr = config.check("angleRepr", angleRepr, "angle representation for transform port");
-    bool hasAngularUnits = config.check("angularUnits", angularUnits, "angular units for transform port");
+    yarp::os::Value * angleRepr, * coordRepr, * angularUnits;
 
-    // check angle representation, leave this block last to allow inner return instruction
-    if (hasCoordRepr || hasAngleRepr || hasAngularUnits)
+    KinRepresentation::coordinate_system coord;
+    KinRepresentation::orientation_system orient;
+    KinRepresentation::angular_units units;
+
+    bool openTransformPort = false;
+
+    if (config.check("coordRepr", coordRepr, "coordinate representation for transform port"))
     {
         std::string coordReprStr = coordRepr->asString();
-        std::string angleReprStr = angleRepr->asString();
-        std::string angularUnitsStr = angularUnits->asString();
-
-        KinRepresentation::coordinate_system coord;
-        KinRepresentation::orientation_system orient;
-        KinRepresentation::angular_units units;
 
         if (!KinRepresentation::parseEnumerator(coordReprStr, &coord))
         {
             CD_WARNING("Unknown coordRepr \"%s\", falling back to default.\n", coordReprStr.c_str());
-            return true;
         }
+        else
+        {
+            openTransformPort = true;
+        }
+    }
+
+    if (config.check("angleRepr", angleRepr, "angle representation for transform port"))
+    {
+        std::string angleReprStr = angleRepr->asString();
 
         if (!KinRepresentation::parseEnumerator(angleReprStr, &orient))
         {
             CD_WARNING("Unknown angleRepr \"%s\", falling back to default.\n", angleReprStr.c_str());
             return true;
         }
+        else
+        {
+            openTransformPort = true;
+        }
+    }
+
+    if (config.check("angularUnits", angularUnits, "angular units for transform port"))
+    {
+        std::string angularUnitsStr = angularUnits->asString();
 
         if (!KinRepresentation::parseEnumerator(angularUnitsStr, &units))
         {
             CD_WARNING("Unknown angularUnits \"%s\", falling back to default.\n", angularUnitsStr.c_str());
             return true;
         }
+        else
+        {
+            openTransformPort = true;
+        }
+    }
 
+    if (openTransformPort)
+    {
         rpcTransformResponder = new RpcTransformResponder(iCartesianControl, coord, orient, units);
-
-        rpcTransformServer.open(prefix + "/rpc_transform:s");
+        ok &= rpcTransformServer.open(prefix + "/rpc_transform:s");
         rpcTransformServer.setReader(*rpcTransformResponder);
     }
 
-    return true;
+    return ok;
 }
 
 // -----------------------------------------------------------------------------