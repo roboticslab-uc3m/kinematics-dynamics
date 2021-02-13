6c7e3fa71dd8b6bf4028fcb1fa5efb096ff7cd97
Juan G. Victores <jgvictores@users.noreply.github.com>
2021-02-13 11:17:48 +0100

    Update exampleKdlSolverFromFile.py
    Minor cleanup

--- examples/python/exampleKdlSolverFromFile.py
+++ examples/python/exampleKdlSolverFromFile.py
@@ -3,11 +3,15 @@
 import yarp
 import kinematics_dynamics
 
+#-- Locate the file with the kinematic chain DH parameters
+
 rf = yarp.ResourceFinder()
 rf.setVerbose(False)
 rf.setDefaultContext("testKdlSolverFromFile")
 rf.setDefaultConfigFile("testKdlSolverFromFile.ini")
 kinematicsFileFullPath = rf.findFileByName("testKdlSolverFromFile.ini")
+
+#-- Load the solver device with this configuration, view the interface
 
 solverOptions = yarp.Property()
 solverOptions.fromConfigFile(kinematicsFileFullPath)
@@ -20,10 +24,11 @@ if not solverDevice.isValid():
     print("Cannot open the device!")
     raise SystemExit
 
-
 cartesianSolver = kinematics_dynamics.viewICartesianSolver(solverDevice) # view the actual interface
 
-# Configuration 1
+##-- Illustration of forward kinematics, for different joint-space positions
+
+print('--- Joint space configuration 1: expect Cartesian space position 0, 0.34692, -0.221806')
 
 q = [0,0,0,0,0,0,0]
 q_vector = yarp.DVector(q)
@@ -31,11 +36,10 @@ x_vector = yarp.DVector()
 
 cartesianSolver.fwdKin(q_vector,x_vector);
 
-print('--- Configuration 1: expect 0, 0.34692, -0.221806')
 print('> fwdKin [%s]' % ', '.join(map(str, q_vector)))
 print('< [%s]' % ', '.join(map(str, x_vector)))
 
-# Configuration 2
+print('--- Joint space configuration 2: expect Cartesian space position 0.718506, 0.34692, 0.4967')
 
 q = [-90,0,0,0,0,0,0]
 q_vector = yarp.DVector(q)
@@ -43,7 +47,6 @@ x_vector = yarp.DVector()
 
 cartesianSolver.fwdKin(q_vector,x_vector);
 
-print('--- Configuration 2: expect 0.718506, 0.34692, 0.4967')
 print('> fwdKin [%s]' % ', '.join(map(str, q_vector)))
 print('< [%s]' % ', '.join(map(str, x_vector)))
 
